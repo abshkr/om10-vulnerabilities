fa9a0418df436b1599ffa33aba75c0fc3d3970d6
Mash Wimalawardana <mashdroidd@gmail.com>
2018-07-31 12:43:52 +1000

    Removed JS

--- JS/scadaviews/bayviewNew/services/baview_db_patches_unload_bay_type.sql
+++
@@ -1,9 +0,0 @@
-
--- Add enums for Loading Bay Types
-
-insert into msg_lookup values(3104, 'ENG', 'Unloading');
-insert into msg_lookup values(3104, 'CHN', '”Õ∆∑–∂‘ÿ');
-
-insert into ENUMITEM values('LOAD_BAY_TYPES', 5, 3104);
-
-commit;
