2b88f456962e1db0ec5db7f3549c530c6b1d610e
Mash Wimalawardana <mashdroidd@gmail.com>
2019-01-31 14:54:22 +1100

    Added new base products

--- react-frontend/src/pages/baseProducts/baseProducts.jsx
+++ react-frontend/src/pages/baseProducts/baseProducts.jsx
@@ -1,11 +1,59 @@
+/**
+ * @description
+ * Base Products Screen
+ * Lets the user perform simple CRUD operations to manipulate the Base Products Data.
+ */
+
 import React, { Component } from "react";
 import auth from "../../utils/auth";
 import Page from "../../components/page";
-import "./baseProducts.css";
+import Filter from "../../components/filter";
+import DataTable from "../../components/table";
+import axios from "axios";
+import search from "../../utils/search";
+import columns from "./columns";
 
 class BaseProducts extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+      data: []
+    };
+  }
+
+  fetchBaseProducts = () => {
+    axios.get(`https://10.1.10.66/api/idassignment/read.php`).then(response => {
+      const data = response.data.records;
+      this.setState({
+        data: data,
+        isLoading: false
+      });
+    });
+  };
+
+  searchObjects = query => {
+    const { value } = query.target;
+    this.setState({
+      filtered: search(value, this.state.data),
+      value
+    });
+  };
+
+  componentDidMount() {
+    this.fetchBaseProducts();
+  }
+
   render() {
-    return <Page page={"Base Products"} isLoading={false} />;
+    const { data, isLoading, filtered, value } = this.state;
+    const results = !!filtered ? filtered : data;
+
+    return (
+      <Page page={"Gantry"} name={"Base Products"} isLoading={isLoading} block={true}>
+        <Filter value={value} search={this.searchObjects} />
+        <DataTable rowKey="kya_key_no" columns={columns(results)} data={results} loading={true} />
+      </Page>
+    );
   }
 }
 
---
+++ react-frontend/src/pages/baseProducts/columns.jsx
@@ -0,0 +1,124 @@
+import React from "react";
+import { Icon } from "antd";
+import { Check, Lock } from "../../constants/colors";
+import Generate from "../../utils/generateOptions";
+
+const columns = data => [
+  {
+    title: "No.",
+    dataIndex: "kya_key_no",
+    key: "kya_key_no",
+    width: 100
+  },
+  {
+    title: "Issuer",
+    dataIndex: "kya_issuer_name",
+    key: "kya_issuer_name",
+    width: 330,
+    filters: Generate(data, "kya_issuer_name"),
+    onFilter: (value, record) => record.kya_issuer_name.indexOf(value) === 0
+  },
+  {
+    title: "Assignment Type",
+    dataIndex: "kya_type_name",
+    key: "kya_type_name",
+    width: 180,
+    filters: Generate(data, "kya_type_name"),
+    onFilter: (value, record) => record.kya_type_name.indexOf(value) === 0
+  },
+  {
+    title: "Physical Type",
+    dataIndex: "kya_phys_name",
+    key: "kya_phys_name",
+    width: 150,
+    filters: Generate(data, "kya_phys_name"),
+    onFilter: (value, record) => record.kya_phys_name.indexOf(value) === 0
+  },
+  {
+    title: "Time Code",
+    dataIndex: "kya_timecode",
+    key: "kya_timecode",
+    width: 150,
+    filters: Generate(data, "kya_timecode"),
+    onFilter: (value, record) => record.kya_timecode.indexOf(value) === 0
+  },
+  {
+    title: "Locked?",
+    dataIndex: "kya_lock",
+    key: "kya_lock",
+    width: 130,
+    filters: [
+      {
+        text: "Locked",
+        value: "Y"
+      },
+      {
+        text: "Unlocked",
+        value: "N"
+      }
+    ],
+    onFilter: (value, record) => record.kya_lock.indexOf(value) === 0,
+    render: text => (
+      <span>
+        <Icon style={{ color: Lock[text], fontSize: 16 }} type={text === "N" ? "unlock" : "lock"} />
+      </span>
+    )
+  },
+  {
+    title: "Adhoc?",
+    dataIndex: "kya_adhoc",
+    key: "kya_adhoc",
+    width: 130,
+    filters: [
+      {
+        text: "Yes",
+        value: "Y"
+      },
+      {
+        text: "No",
+        value: "N"
+      }
+    ],
+    onFilter: (value, record) => record.kya_adhoc.indexOf(value) === 0,
+    render: text => (
+      <span>
+        <Icon style={{ color: Check[text], fontSize: 16 }} type={text === "N" ? "close" : "check"} />
+      </span>
+    )
+  },
+  {
+    title: "Tag Text",
+    dataIndex: "kya_txt",
+    key: "kya_txt",
+    width: 150
+  },
+  {
+    title: "Personnel",
+    dataIndex: "kya_psnl_name",
+    key: "kya_psnl_name",
+    width: 160
+  },
+  {
+    title: "Role",
+    dataIndex: "kya_role_name",
+    key: "kya_role_name",
+    filters: Generate(data, "kya_role_name"),
+    onFilter: (value, record) => String(record.kya_role_name).indexOf(value) === 0
+  },
+  {
+    title: "Drawer",
+    dataIndex: "kya_draw_name",
+    key: "kya_draw_name",
+    filters: Generate(data, "kya_draw_name"),
+    onFilter: (value, record) => String(record.kya_draw_name).indexOf(value) === 0
+  },
+  {
+    title: "Supplier",
+    dataIndex: "kya_supp_name",
+    key: "kya_supp_name",
+    filters: Generate(data, "kya_supp_name"),
+    onFilter: (value, record) => String(record.kya_supp_name).indexOf(value) === 0
+  }
+];
+
+export default columns;
