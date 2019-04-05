8638edc039d6818bec376c8948e9873d4af8b2f4
cw <caowei@diamondkey.com>
2019-04-06 00:02:09 +1100

    formatted

--- backend/api/base_prod/create.php
+++ backend/api/base_prod/create.php
@@ -1,6 +1,6 @@
 <?php
 // required headers
-include_once '../shared/header.php'; 
+include_once '../shared/header.php';
 
 include_once '../config/database.php';
 include_once '../objects/base_product.php';
