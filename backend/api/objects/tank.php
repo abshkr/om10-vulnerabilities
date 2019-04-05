48a50a0d15e00c3fd0d986f7b89778c2c0b84bf2
cw <caowei@diamondkey.com>
2019-04-05 16:57:26 +1100

    formatted by phpfmt

--- backend/api/objects/tank.php
+++ backend/api/objects/tank.php
@@ -1,12 +1,12 @@
 <?php
 
-include_once __DIR__  . '/../config/journal.php';
-include_once __DIR__  . '/../config/log.php';
-include_once __DIR__  . '/../shared/utilities.php';
+include_once __DIR__ . '/../config/journal.php';
+include_once __DIR__ . '/../config/log.php';
+include_once __DIR__ . '/../shared/utilities.php';
 
 class Tank
 {
-	// database connection and table name
+    // database connection and table name
     private $conn;
 
     // constructor with $db as database connection
@@ -18,7 +18,7 @@ class Tank
     //Because base cannot be too many, do not do limit
     //Old sample from amf TankService.php::getPaged():
     // "tank_code":"ST 3","tank_name":"ST 3","tank_terminal":"TGI","tank_sitename":"Shell TanjungGelang","tank_base":"220008581","tank_base_name":"Nemo 2016","tank_base_group":null,"tank_base_class":"6","tank_bclass_name":"Additive","tank_base_tunit":"0","tank_base_rpttemp":"0","tank_bclass_dens_lo":"1","tank_bclass_dens_hi":"2000","tank_bclass_vcf_alg":"3","tank_bclass_temp_lo":"-20","tank_bclass_temp_hi":"200","tank_drv_type":null,"tank_drv_aux":null,"tank_identifier":null,"tank_location":null,"tank_outflow_ope":null,"tank_inflow_open":null,"tank_adhoc_ivrq":null,"tank_inv_needed":null,"tank_dipping_on":"N","tank_leakdtct_on":null,"tank_alarmed":null,"tank_poll_gap":"0","tank_prod_lvl":"0","tank_address":"0","tank_rcpts":"0","tank_trfs":"93526","tank_no_sbt":"0","tank_versno":"0","tank_pakscan_act":"0","tank_alarm_state":null,"tank_lvl_alarm":"0","tank_lvlalarm_desc":"OK - NORMAL","tank_gaugingmthd":"0","tank_gaugingmthd_desc":"MANUAL","tank_instance":"0","tank_channel":"0","tank_sbt_ty":"0","tank_eth_content":"0","tank_ltr_close":"0","tank_kg_close":"0","tank_close_dens":"980","tank_rptvcfclose":"1","tank_inflow_rate":"0","tank_spare_fld1":null,"tank_spare_fld2":null,"tank_rcpt_vol":"0","tank_trf_vol":"0","tank_rcpt_kg":"0","tank_trf_kg":"0","tank_pump_vol":"0","tank_res":"0","tank_amb_vol":"0","tank_cor_vol":"0","tank_vapour_kg":"0","tank_liquid_kg":"0","tank_water":"0","tank_water_lvl":"0","tank_ullage":"0","tank_api":"12.8","tank_prod_c_of_e":"0","tank_60_86_vcf":"0","tank_density":"980","tank_temp":"0","tank_rptvcf":".0001","tank_amb_density":"0","tank_dtol_volume":"0","tank_dtol_percent":"0","tank_mtol_volume":"0","tank_mtol_percent":"0","tank_date":null,"tank_group":null,"tank_15_density":"980","tank_base_ref_temp":null,"tank_base_ref_tunt":null,"tank_base_corr_mthd":"1","tank_base_ref_temp_spec":"1","tank_base_limit_preset_ht":null,"tank_base_dens_lo":"1","tank_base_dens_hi":"2000","tank_base_color":null,"tank_active":"1","tank_atg_manchg":"2018-11-01 17:12:56:61976","tank_atg_status":null,"tank_sulphur":null,"tank_flashpoint":null,"tank_status":"0","tank_status_name":"In Service - Not used","tank_hh_level":null,"tank_h_level":null,"tank_l_level":null,"tank_ll_level":null,"tank_uh_level":null,"tank_ul_level":null,"tank_hh_state":"-1","tank_h_state":"-1","tank_l_state":"-1","tank_ll_state":"-1","tank_uh_state":"-1","tank_ul_state":"-1"
-    function read()
+    public function read()
     {
         // if (!isset($this->end_num)) {
         //     $this->start_num = 1;
@@ -28,7 +28,7 @@ class Tank
         Utilities::sanitize($this);
 
         $query = "
-            SELECT * FROM GUI_TANKS ORDER BY TANK_CODE";        
+            SELECT * FROM GUI_TANKS ORDER BY TANK_CODE";
         $stmt = oci_parse($this->conn, $query);
         if (oci_execute($stmt)) {
             return $stmt;
@@ -40,7 +40,7 @@ class Tank
 
     public function create()
     {
-        write_log(sprintf("%s::%s() START", __CLASS__, __FUNCTION__), 
+        write_log(sprintf("%s::%s() START", __CLASS__, __FUNCTION__),
             __FILE__, __LINE__);
 
         Utilities::sanitize($this);
@@ -107,7 +107,11 @@ class Tank
                 TANK_DAILY_TOL_VOL,
                 TANK_MONTHLY_TOL_VOL,
                 TANK_MONTHLY_TOL_PERCENT,
-                TANK_15_DENSITY
+                TANK_15_DENSITY,
+                TANK_EXCL_FROM_PID,
+                TANK_EXCL_FROM_PDS,
+                TANK_EXCL_FROM_SPECIAL_MV,
+                TANK_EXCL_FROM_STOCK_REP
                 )
             VALUES (
                 :tank_code,
@@ -170,7 +174,11 @@ class Tank
                 :tank_dtol_volume,
                 :tank_mtol_volume,
                 :tank_mtol_percent,
-                :tank_15_density
+                :tank_15_density,
+                :tank_excl_from_pid,
+                :tank_excl_from_pds,
+                :tank_excl_from_special_mv,
+                :tank_excl_from_stock_rep
                 )";
         $stmt = oci_parse($this->conn, $query);
         oci_bind_by_name($stmt, ':tank_name', $this->tank_name);
@@ -184,6 +192,10 @@ class Tank
         oci_bind_by_name($stmt, ':tank_mtol_percent', $this->tank_mtol_percent);
         oci_bind_by_name($stmt, ':tank_api', $this->tank_api);
         oci_bind_by_name($stmt, ':tank_15_density', $this->tank_15_density);
+        oci_bind_by_name($stmt, ':tank_excl_from_pid', $this->tank_excl_from_pid);
+        oci_bind_by_name($stmt, ':tank_excl_from_pds', $this->tank_excl_from_pds);
+        oci_bind_by_name($stmt, ':tank_excl_from_special_mv', $this->tank_excl_from_special_mv);
+        oci_bind_by_name($stmt, ':tank_excl_from_stock_rep', $this->tank_excl_from_stock_rep);
         if (!oci_execute($stmt, OCI_NO_AUTO_COMMIT)) {
             write_log("DB error:" . oci_error($stmt)['message'], __FILE__, __LINE__, LogLevel::ERROR);
             oci_rollback($this->conn);
@@ -192,15 +204,14 @@ class Tank
 
         $journal = new Journal($this->conn, false);
         $curr_psn = Utilities::getCurrPsn();
-        $jnl_data[0] = $curr_psn; 
+        $jnl_data[0] = $curr_psn;
         $jnl_data[1] = "Tank";
         $jnl_data[2] = $this->tank_code;
         $jnl_data[3] = sprintf(
             "name:%s, base product:%s", $this->tank_name, $this->tank_base);
 
         if (!$journal->jnlLogEvent(
-            Lookup::RECORD_ADDED, $jnl_data, JnlEvent::JNLT_CONF, JnlClass::JNLC_EVENT))
-        {
+            Lookup::RECORD_ADDED, $jnl_data, JnlEvent::JNLT_CONF, JnlClass::JNLC_EVENT)) {
             write_log("DB error:" . oci_error($stmt)['message'], __FILE__, __LINE__, LogLevel::ERROR);
             oci_rollback($this->conn);
             return false;
@@ -212,18 +223,18 @@ class Tank
 
     public function update()
     {
-        write_log(sprintf("%s::%s() START", __CLASS__, __FUNCTION__), 
+        write_log(sprintf("%s::%s() START", __CLASS__, __FUNCTION__),
             __FILE__, __LINE__);
 
         Utilities::sanitize($this);
 
         $query = "
-            SELECT * FROM GUI_TANKS 
+            SELECT * FROM GUI_TANKS
             WHERE TANK_CODE = :tank_code";
         $stmt = oci_parse($this->conn, $query);
         oci_bind_by_name($stmt, ':tank_code', $this->tank_code);
         if (oci_execute($stmt, OCI_NO_AUTO_COMMIT)) {
-            $row = oci_fetch_array($stmt, OCI_ASSOC + OCI_RETURN_NULLS);            
+            $row = oci_fetch_array($stmt, OCI_ASSOC + OCI_RETURN_NULLS);
         } else {
             write_log("DB error:" . oci_error($stmt)['message'], __FILE__, __LINE__, LogLevel::ERROR);
         }
@@ -239,7 +250,11 @@ class Tank
                 TANK_DAILY_TOL_VOL = :tank_dtol_volume,
                 TANK_MONTHLY_TOL_VOL = :tank_mtol_volume,
                 TANK_MONTHLY_TOL_PERCENT = :tank_mtol_percent,
-                TANK_15_DENSITY = :tank_15_density
+                TANK_15_DENSITY = :tank_15_density,
+                TANK_EXCL_FROM_PID = :tank_excl_from_pid,
+                TANK_EXCL_FROM_PDS = :tank_excl_from_pds,
+                TANK_EXCL_FROM_SPECIAL_MV = :tank_excl_from_special_mv,
+                TANK_EXCL_FROM_STOCK_REP = :tank_excl_from_stock_rep
             WHERE TANK_CODE = :tank_code";
         $stmt = oci_parse($this->conn, $query);
         oci_bind_by_name($stmt, ':tank_name', $this->tank_name);
@@ -252,7 +267,11 @@ class Tank
         oci_bind_by_name($stmt, ':tank_mtol_volume', $this->tank_mtol_volume);
         oci_bind_by_name($stmt, ':tank_mtol_percent', $this->tank_mtol_percent);
         oci_bind_by_name($stmt, ':tank_api', $this->tank_api);
-        oci_bind_by_name($stmt, ':tank_15_density', $this->tank_15_density);       
+        oci_bind_by_name($stmt, ':tank_15_density', $this->tank_15_density);
+        oci_bind_by_name($stmt, ':tank_excl_from_pid', $this->tank_excl_from_pid);
+        oci_bind_by_name($stmt, ':tank_excl_from_pds', $this->tank_excl_from_pds);
+        oci_bind_by_name($stmt, ':tank_excl_from_special_mv', $this->tank_excl_from_special_mv);
+        oci_bind_by_name($stmt, ':tank_excl_from_stock_rep', $this->tank_excl_from_stock_rep);
         if (!oci_execute($stmt, OCI_NO_AUTO_COMMIT)) {
             write_log("DB error:" . oci_error($stmt)['message'], __FILE__, __LINE__, LogLevel::ERROR);
             oci_rollback($this->conn);
@@ -261,13 +280,12 @@ class Tank
 
         $journal = new Journal($this->conn, false);
         $curr_psn = Utilities::getCurrPsn();
-        $jnl_data[0] = $curr_psn; 
+        $jnl_data[0] = $curr_psn;
         $jnl_data[1] = "Tank";
         $jnl_data[2] = $this->tank_code;
 
         if (!$journal->jnlLogEvent(
-            Lookup::RECORD_ALTERED, $jnl_data, JnlEvent::JNLT_CONF, JnlClass::JNLC_EVENT))
-        {
+            Lookup::RECORD_ALTERED, $jnl_data, JnlEvent::JNLT_CONF, JnlClass::JNLC_EVENT)) {
             write_log("DB error:" . oci_error($stmt)['message'], __FILE__, __LINE__, LogLevel::ERROR);
             oci_rollback($this->conn);
             return false;
@@ -276,7 +294,7 @@ class Tank
         $module = "GUI_TANKS";
         $record = sprintf("code:%s", $this->tank_code);
         foreach ($this as $key => $value) {
-            if (isset($row[strtoupper($key)]) && $value != $row[strtoupper($key)] && 
+            if (isset($row[strtoupper($key)]) && $value != $row[strtoupper($key)] &&
                 !$journal->valueChange(
                     $module, $record, strtoupper($key), $row[strtoupper($key)], $value)) {
                 oci_rollback($this->conn);
@@ -290,7 +308,7 @@ class Tank
 
     public function delete()
     {
-        write_log(sprintf("%s::%s() START. base_code:%s", __CLASS__, __FUNCTION__, $this->tank_code), 
+        write_log(sprintf("%s::%s() START. base_code:%s", __CLASS__, __FUNCTION__, $this->tank_code),
             __FILE__, __LINE__);
 
         Utilities::sanitize($this);
@@ -300,13 +318,13 @@ class Tank
             WHERE TGR_TKLK_TANKCODE = :tank_code";
         $stmt = oci_parse($this->conn, $query);
         oci_bind_by_name($stmt, ':tank_code', $this->tank_code);
-        if (!oci_execute($stmt, OCI_NO_AUTO_COMMIT)) {            
+        if (!oci_execute($stmt, OCI_NO_AUTO_COMMIT)) {
             write_log("DB error:" . oci_error($stmt)['message'], __FILE__, __LINE__, LogLevel::ERROR);
             return false;
         }
 
         $query = "
-            DELETE FROM TANKS 
+            DELETE FROM TANKS
             WHERE TANK_CODE = :tank_code";
         $stmt = oci_parse($this->conn, $query);
         oci_bind_by_name($stmt, ':tank_code', $this->tank_code);
@@ -318,14 +336,13 @@ class Tank
 
         $journal = new Journal($this->conn, false);
         $curr_psn = Utilities::getCurrPsn();
-        $jnl_data[0] = $curr_psn; 
+        $jnl_data[0] = $curr_psn;
         $jnl_data[1] = "Tank";
         $jnl_data[2] = $this->tank_code;
         $jnl_data[3] = "";
 
         if (!$journal->jnlLogEvent(
-            Lookup::RECORD_DELETED, $jnl_data, JnlEvent::JNLT_CONF, JnlClass::JNLC_EVENT))
-        {
+            Lookup::RECORD_DELETED, $jnl_data, JnlEvent::JNLT_CONF, JnlClass::JNLC_EVENT)) {
             write_log("DB error:" . oci_error($stmt)['message'], __FILE__, __LINE__, LogLevel::ERROR);
             oci_rollback($this->conn);
             return false;
