98c06b4ce5843d4892a6957133e4c391dd589737
cw <caowei@diamondkey.com>
2019-06-06 13:42:42 +1000

    Update

--- server/api/pages/personnel/time_codes.php
+++ server/api/pages/personnel/time_codes.php
@@ -1,21 +1,22 @@
 <?php
 // required headers
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
- 
-// include database and object files
-include_once '../../config/database.php';
-include_once '../../objects/time_code.php';
- 
-// instantiate database
-$database = new Database();
-$db = $database->getConnection();
-$time_code = new TimeCode($db);
-$stmt = $time_code->read();
- 
-$personnels_arr = array();
-$personnels_arr["records"] = array();
+include_once '../../shared/header.php';
 
-// retrieve our table contents
-$num = Utilities::retrieve($personnels_arr["records"], $stmt);
-Utilities::echoRead($num, $personnels_arr, "time code");
+// include database and object files
+// include_once '../../config/database.php';
+include_once '../../objects/time_code.php';
+
+Utilities::read('TimeCode', "read", $filter = true);
+
+// // instantiate database
+// $database = new Database();
+// $db = $database->getConnection();
+// $time_code = new TimeCode($db);
+// $stmt = $time_code->read();
+
+// $personnels_arr = array();
+// $personnels_arr["records"] = array();
+
+// // retrieve our table contents
+// $num = Utilities::retrieve($personnels_arr["records"], $stmt);
+// Utilities::echoRead($num, $personnels_arr, "time code");
