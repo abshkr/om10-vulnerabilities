aa2bac7b7c6527349ac89c6c3a94088729afb3c6
cw <caowei@diamondkey.com>
2019-06-07 23:25:15 +1000

    update

--- server/api/pages/personnel/time_codes.php
+++ server/api/pages/personnel/time_codes.php
@@ -7,16 +7,3 @@ include_once '../../shared/header.php';
 include_once '../../objects/time_code.php';
 
 Utilities::read('TimeCode', "read", $filter = true);
-
-// // instantiate database
-// $database = new Database();
-// $db = $database->getConnection();
-// $time_code = new TimeCode($db);
-// $stmt = $time_code->read();
-
-// $personnels_arr = array();
-// $personnels_arr["records"] = array();
-
-// // retrieve our table contents
-// $num = Utilities::retrieve($personnels_arr["records"], $stmt);
-// Utilities::echoRead($num, $personnels_arr, "time code");
