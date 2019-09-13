0b1cb217e0e79534434e1c1c34a5b5af14bb7c9d
Jack Zhu <50163019+jackgithb@users.noreply.github.com>
2019-09-13 14:06:04 +1000

    Update eqpt_type.php

--- server/api/objects/eqpt_type.php
+++ server/api/objects/eqpt_type.php
@@ -56,7 +56,8 @@ class EquipmentType extends CommonClass
             $row = oci_fetch_array($stmt, OCI_ASSOC + OCI_RETURN_NULLS);
             return (int) $row['CN'];
         } else {
-            write_log("DB error:" . oci_error($stmt)['message'], __FILE__, __LINE__, LogLevel::ERROR);
+            $e = oci_error($stmt);
+            write_log("DB error:" . $e['message'], __FILE__, __LINE__, LogLevel::ERROR);
             return 0;
         }
     }
@@ -80,7 +81,8 @@ class EquipmentType extends CommonClass
         if (oci_execute($stmt)) {
             return $stmt;
         } else {
-            write_log("DB error:" . oci_error($stmt)['message'], __FILE__, __LINE__, LogLevel::ERROR);
+            $e = oci_error($stmt);
+            write_log("DB error:" . $e['message'], __FILE__, __LINE__, LogLevel::ERROR);
             return null;
         }
     }
