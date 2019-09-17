9bedcb3d907342096612e3a46e8d562fc9fa04eb
Mash Wimalawardana <mashdroidd@gmail.com>
2019-09-17 12:58:30 +1000

    Added Translations.

--- client/src/auth/auth.jsx
+++ client/src/auth/auth.jsx
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
-import axios from "axios";
-import { ROUTES } from "../constants";
-import { connect } from "react-redux";
-import { Loading } from "../components";
-import { api } from "../api";
-import { withTranslation } from "react-i18next";
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import { connect } from 'react-redux';
+import { Loading } from '../components';
+import { api } from '../api';
+import { withTranslation } from 'react-i18next';
 
 export default Module => {
   class ComposedComponent extends Component {
@@ -13,27 +13,8 @@ export default Module => {
       isLoading: true
     };
 
-    checkFrame = () => {
-      const { history } = this.props;
-
-      if (process.env.NODE_ENV === "development") {
-        if (window !== window.top) {
-          history.push(ROUTES.UNAUTHORIZED);
-        }
-      } else {
-        if (window === window.top) {
-          history.push(ROUTES.UNAUTHORIZED);
-        }
-      }
-    };
-
     getConfiguration = () => {
-      const url =
-        process.env.NODE_ENV === "development"
-          ? "/config.json"
-          : `https://${api}/api/config.php`;
-
-      axios.get(url).then(response => {
+      axios.get('/config.php').then(response => {
         this.setState({
           configuration: response.data,
           isLoading: false
@@ -42,12 +23,7 @@ export default Module => {
     };
 
     componentDidMount() {
-      this.checkFrame();
       this.getConfiguration();
-    }
-
-    componentDidUpdate() {
-      this.checkFrame();
     }
 
     render() {
