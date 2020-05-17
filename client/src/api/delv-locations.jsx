a0fea7863e4d02b3cc25ad4a3dea7f366be31a86
Bin Zhou <binzhou@diamondkey.com>
2020-05-18 07:04:33 +0800

    Added screen Delivery Locations and finished VIEW functionality

---
+++ client/src/api/delv-locations.jsx
@@ -0,0 +1,18 @@
+export const READ = '/api/pages/delv_location/read.php';
+export const DOC_TYPES = '/api/pages/delv_location/document_types.php';
+export const EQPT_TYPES = '/api/pages/delv_location/eqpt_types.php';
+export const TRANSPORT_TYPES = '/api/pages/delv_location/transport_types.php';
+export const UNIT_TYPES = '/api/pages/delv_location/unit_types.php';
+/*
+export const LOCKS = '/api/pages/delv_location/lock_types.php';
+export const PERIOD = '/api/pages/delv_location/period_types.php';
+export const SUPPLIERS = '/api/pages/delv_location/suppliers.php';
+export const DRAWERS = '/api/pages/delv_location/drawers.php';
+export const CARRIERS = '/api/pages/delv_location/carriers.php';
+export const CUSTOMERS = '/api/pages/delv_location/customers.php';
+export const ITEMS = '/api/pages/delv_location/alloc_items.php';
+export const RESET = '/api/pages/delv_location/reset.php ';
+*/
+export const CREATE = '/api/pages/delv_location/create.php';
+export const UPDATE = '/api/pages/delv_location/update.php';
+export const DELETE = '/api/pages/delv_location/delete.php';
---
+++ client/src/pages/delv-locations/delv-locations.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+
+import useSWR from 'swr';
+import { Button } from 'antd';
+import { useTranslation } from 'react-i18next';
+import { SyncOutlined, PlusOutlined } from '@ant-design/icons';
+
+import { Page, DataTable, Download } from '../../components';
+import { DELV_LOCATIONS } from '../../api';
+import { useAuth } from '../../hooks';
+import columns from './columns';
+import auth from '../../auth';
+
+//import Forms from './forms';
+
+const DelvLocations = () => {
+  const [visible, setVisible] = useState(false);
+  const [selected, setSelected] = useState(null);
+
+  const { t } = useTranslation();
+
+  const access = useAuth('M_DELIVERYLOCATIONS');
+
+  const { data: payload, isValidating, revalidate } = useSWR(DELV_LOCATIONS.READ);
+
+  const handleFormState = (visibility, value) => {
+    setVisible(visibility);
+    setSelected(value);
+  };
+
+  const fields = columns(t);
+
+  const data = payload?.records;
+  const isLoading = isValidating || !data;
+
+  const page = t('pageMenu.customers');
+  const name = t('pageNames.deliveryLocations');
+
+  const modifiers = (
+    <>
+      <Button icon={<SyncOutlined />} onClick={() => revalidate()} loading={isLoading}>
+        {t('operations.refresh')}
+      </Button>
+
+      <Download data={data} isLoading={isLoading} columns={fields} />
+
+      <Button
+        type="primary"
+        icon={<PlusOutlined />}
+        onClick={() => handleFormState(true, null)}
+        loading={isLoading}
+        disabled={!access.canCreate}
+      >
+        {t('operations.create')}
+      </Button>
+    </>
+  );
+
+  return (
+    <Page page={page} name={name} modifiers={modifiers} access={access}>
+      <DataTable
+        data={data}
+        columns={fields}
+        isLoading={isLoading}
+        selectionMode="single"
+        onClick={(payload) => handleFormState(true, payload)}
+        handleSelect={(payload) => handleFormState(true, payload[0])}
+      />
+    </Page>
+  );
+      //<Forms value={selected} visible={visible} handleFormState={handleFormState} access={access} />
+};
+
+export default auth(DelvLocations);
