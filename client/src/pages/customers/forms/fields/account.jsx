320f1376875ab8ded906658a34ad77da99e7d9d3
Bin Zhou <binzhou@diamondkey.com>
2020-05-24 18:26:21 +0800

    customer account number field

--- client/src/pages/customers/forms/fields/account.jsx
+++ client/src/pages/customers/forms/fields/account.jsx
@@ -1,10 +1,9 @@
+/*
 import React, { useEffect, useState } from 'react';
-
 import useSWR from 'swr';
 import { useTranslation } from 'react-i18next';
 import { Form, Input } from 'antd';
 import _ from 'lodash';
-
 import { CUSTOMERS } from '../../../../api';
 
 const validateSwitch = (isLoading, match) => {
@@ -20,9 +19,7 @@ const validateSwitch = (isLoading, match) => {
 const Account = ({ form, value }) => {
   const [account, setAccount] = useState(value?.cust_account);
   const [matched, setMatched] = useState(false);
-
   const { t } = useTranslation();
-
   const { setFieldsValue, validateFields } = form;
 
   const { data, isValidating } = useSWR(`${CUSTOMERS.CHECK_ACCOUNT}?cust_account=${account}`, {
@@ -41,7 +38,6 @@ const Account = ({ form, value }) => {
     if (data) {
       const counter = _.toNumber(data?.records[0]?.cnt);
       const match = counter > 0;
-
       setMatched(match);
     }
   }, [data]);
@@ -56,15 +52,12 @@ const Account = ({ form, value }) => {
     if (matched && !value) {
       return Promise.reject(t('descriptions.alreadyExists'));
     }
-
     if (input === '' || !input) {
       return Promise.reject(`${t('validate.set')} ─ ${t('fields.custAccount')}`);
     }
-
     if (input && input.length > 40) {
       return Promise.reject(`${t('placeholder.maxCharacters')}: 40 ─ ${t('descriptions.maxCharacters')}`);
     }
-
     return Promise.resolve();
   };
 
@@ -84,6 +77,59 @@ const Account = ({ form, value }) => {
       shouldUpdate
     >
       <Input disabled={!!value} onChange={handleFieldChange} />
+    </Form.Item>
+  );
+};
+
+export default Account;
+*/
+import React, { useEffect } from 'react';
+
+import useSWR from 'swr';
+import { useTranslation } from 'react-i18next';
+import { Form, Input } from 'antd';
+import _ from 'lodash';
+
+import { CUSTOMERS } from '../../../../api';
+
+const Account = ({ form, value }) => {
+  const { t } = useTranslation();
+
+  const { setFieldsValue } = form;
+
+  const { data: accounts, isValidating } = useSWR(CUSTOMERS.ACCOUNT_NUMBERS);
+
+  useEffect(() => {
+    if (value) {
+      setFieldsValue({
+        cust_account: value.cust_account
+      });
+    }
+  }, [value, setFieldsValue]);
+
+  const validate = (rule, input) => {
+    const match = _.find(accounts?.records, record => {
+      return record.cust_acct.toLowerCase() === input?.toLowerCase();
+    });
+
+    if (!!match && !value) {
+      return Promise.reject(t('descriptions.alreadyExists'));
+    }
+
+    if (input === '' || !input) {
+      return Promise.reject(`${t('validate.set')} ─ ${t('fields.custAccount')}`);
+    }
+
+    if (input && input.length > 40) {
+      return Promise.reject(`${t('placeholder.maxCharacters')}: 40 ─ ${t('descriptions.maxCharacters')}`);
+    }
+
+    return Promise.resolve();
+  };
+
+  return (
+    <Form.Item name="cust_account" label={t('fields.custAccount')} rules={[{ required: true, validator: validate }]}>
+      <Input disabled={!!value || isValidating} />
     </Form.Item>
   );
 };
