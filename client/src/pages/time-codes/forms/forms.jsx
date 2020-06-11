a3cb68dc3e6cbd490b11136c3ee1d6c8c9833120
WeiCaoMelbourne <weicao.melbourne@gmail.com>
2020-06-11 18:31:06 +1000

    OM5K-6515 Time code screen should have the ability to save different time code setting.

---
+++ client/src/pages/time-codes/forms/forms.jsx
@@ -0,0 +1,119 @@
+import React from 'react';
+
+import {
+  EditOutlined,
+  PlusOutlined,
+  QuestionCircleOutlined,
+  CloseOutlined,
+} from '@ant-design/icons';
+import { Form, Button, Tabs, Modal, notification, Drawer, Input } from 'antd';
+import { useTranslation } from 'react-i18next';
+import { mutate } from 'swr';
+import axios from 'axios';
+import _ from 'lodash';
+import { TIME_CODES } from '../../../api';
+
+const TabPane = Tabs.TabPane;
+
+const TimecodeForm = ({ value, visible, handleFormState }) => {
+  const { t } = useTranslation();
+  const [form] = Form.useForm();
+
+  const IS_CREATING = !value;
+  
+  const onComplete = () => {
+    handleFormState(false, null); 
+    mutate(TIME_CODES.READ);
+  };
+
+  const onFinish = async () => {
+    const values = await form.validateFields();
+
+    Modal.confirm({
+      title: t('prompts.create'),
+      okText: t('operations.create'),
+      okType: 'primary',
+      icon: <QuestionCircleOutlined />,
+      cancelText: t('operations.no'),
+      centered: true,
+      onOk: async () => {
+        await axios
+          .post(TIME_CODES.CREATE, values)
+          .then(() => {
+            onComplete();
+
+            notification.success({
+              message: t('messages.createSuccess'),
+              description: t('descriptions.createSuccess'),
+            });
+          })
+          .catch((errors) => {
+            _.forEach(errors.response.data.errors, (error) => {
+              notification.error({
+                message: error.type,
+                description: error.message,
+              });
+            });
+          });
+      },
+    });
+  };
+
+  const validate = (rule, input) => {
+    if (input === '' || !input) {
+      return Promise.reject(`${t('fields.timeCode')}`);
+    }
+
+    if (input && input.length > 4) {
+      return Promise.reject(`${t('placeholder.maxCharacters')}: 4 ─ ${t('descriptions.maxCharacters')}`);
+    }
+
+    return Promise.resolve();
+  };
+
+  return (
+    <Drawer
+      bodyStyle={{ paddingTop: 5 }}
+      onClose={() => handleFormState(false, null)}
+      maskClosable={IS_CREATING}
+      destroyOnClose={true}
+      mask={IS_CREATING}
+      placement="right"
+      width="40vw"
+      visible={visible}
+      footer={
+        <>
+          <Button
+            htmlType="button"
+            icon={<CloseOutlined />}
+            style={{ float: 'right' }}
+            onClick={() => handleFormState(false, null)}
+          >
+            {t('operations.cancel')}
+          </Button>
+
+          <Button
+            type="primary"
+            icon={IS_CREATING ? <PlusOutlined /> : <EditOutlined />}
+            onClick={onFinish}
+            style={{ float: 'right', marginRight: 5 }}
+          >
+            {t('operations.create')}
+          </Button>
+        </>
+      }
+    >
+      <Form layout="vertical" form={form} scrollToFirstError>
+        <Tabs defaultActiveKey="1">
+          <TabPane tab={t('tabColumns.general')} key="1">
+            <Form.Item name="tcd_title" label={t('fields.timeCode')} rules={[{ required: true, validator: validate }]}>
+                <Input></Input>
+            </Form.Item>
+          </TabPane>
+        </Tabs>
+      </Form>
+    </Drawer>
+  );
+};
+
+export default TimecodeForm;
