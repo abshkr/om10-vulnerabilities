1788ef8191a1ecdbf4ef9b97647e9a901007e0fe
WeiCaoMelbourne <weicao.melbourne@gmail.com>
2020-06-14 00:54:25 +1000

    OM5K-6445 Bay Type field cannot find Tranditional Loading option on the Company Bay Movement screen

--- client/src/pages/company-bay-movement/forms/forms.jsx
+++ client/src/pages/company-bay-movement/forms/forms.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import {
   EditOutlined,
@@ -8,23 +8,32 @@ import {
   QuestionCircleOutlined,
 } from '@ant-design/icons';
 
-import { Form, Button, Tabs, Modal, notification } from 'antd';
+import { Form, Button, Tabs, Modal, notification, Drawer } from 'antd';
 import { useTranslation } from 'react-i18next';
 import { mutate } from 'swr';
 import axios from 'axios';
+import _ from 'lodash';
 
 import Fields from './fields';
 import { COMPANY_BAY_MOVEMENT } from '../../../api';
 
 const TabPane = Tabs.TabPane;
 
-const FormModal = ({ value }) => {
+const FormModal = ({ value, visible, handleFormState, auth }) => {
   const { t } = useTranslation();
   const [form] = Form.useForm();
+  const { resetFields } = form;
 
   const IS_CREATING = !value;
 
-  const onFinish = (values) => {
+  const onComplete = () => {
+    handleFormState(false, null); 
+    mutate(COMPANY_BAY_MOVEMENT.READ);
+  };
+
+  const onFinish = async () => {
+    const values = await form.validateFields();
+
     Modal.confirm({
       title: IS_CREATING ? t('prompts.create') : t('prompts.update'),
       okText: IS_CREATING ? t('operations.create') : t('operations.update'),
@@ -37,9 +46,7 @@ const FormModal = ({ value }) => {
           .post(IS_CREATING ? COMPANY_BAY_MOVEMENT.CREATE : COMPANY_BAY_MOVEMENT.UPDATE, values)
           .then(
             axios.spread((response) => {
-              Modal.destroyAll();
-
-              mutate(COMPANY_BAY_MOVEMENT.READ);
+              onComplete();
 
               notification.success({
                 message: IS_CREATING ? t('messages.createSuccess') : t('messages.updateSuccess'),
@@ -47,10 +54,12 @@ const FormModal = ({ value }) => {
               });
             })
           )
-          .catch((error) => {
-            notification.error({
-              message: error.message,
-              description: IS_CREATING ? t('descriptions.createFailed') : t('descriptions.updateFailed'),
+          .catch((errors) => {
+            _.forEach(errors.response.data.errors, (error) => {
+              notification.error({
+                message: error.type,
+                description: error.message,
+              });
             });
           });
       },
@@ -69,63 +78,87 @@ const FormModal = ({ value }) => {
           .post(COMPANY_BAY_MOVEMENT.DELETE, value)
           .then(
             axios.spread((response) => {
-              mutate(COMPANY_BAY_MOVEMENT.READ);
-              Modal.destroyAll();
+              onComplete();
+
               notification.success({
                 message: t('messages.deleteSuccess'),
                 description: `${t('descriptions.deleteSuccess')}`,
               });
             })
           )
-          .catch((error) => {
-            notification.error({
-              message: error.message,
-              description: t('descriptions.deleteFailed'),
+          .catch((errors) => {
+            _.forEach(errors.response.data.errors, (error) => {
+              notification.error({
+                message: error.type,
+                description: error.message,
+              });
             });
           });
       },
     });
   };
 
+  useEffect(() => {
+    if (!value && !visible) {
+      resetFields();
+    } 
+  }, [value, visible]);
+
   return (
-    <Form layout="vertical" form={form} onFinish={onFinish} scrollToFirstError>
-      <Tabs defaultActiveKey="1">
-        <TabPane tab={t('tabColumns.general')} key="1">
-          <Fields form={form} value={value} />
-        </TabPane>
-      </Tabs>
-
-      <Form.Item>
-        <Button
-          htmlType="button"
-          icon={<CloseOutlined />}
-          style={{ float: 'right' }}
-          onClick={() => Modal.destroyAll()}
-        >
-          {t('operations.cancel')}
-        </Button>
-
-        <Button
-          type="primary"
-          icon={IS_CREATING ? <EditOutlined /> : <PlusOutlined />}
-          htmlType="submit"
-          style={{ float: 'right', marginRight: 5 }}
-        >
-          {IS_CREATING ? t('operations.create') : t('operations.update')}
-        </Button>
-
-        {!IS_CREATING && (
+    <Drawer
+      bodyStyle={{ paddingTop: 5 }}
+      onClose={() => handleFormState(false, null)}
+      maskClosable={IS_CREATING}
+      destroyOnClose={true}
+      mask={IS_CREATING}
+      placement="right"
+      width="50vw"
+      visible={visible}
+      footer={
+        <>
           <Button
-            type="danger"
-            icon={<DeleteOutlined />}
-            style={{ float: 'right', marginRight: 5 }}
-            onClick={onDelete}
+            htmlType="button"
+            icon={<CloseOutlined />}
+            style={{ float: 'right' }}
+            onClick={() => handleFormState(false, null)}
           >
-            {t('operations.delete')}
+            {t('operations.cancel')}
           </Button>
-        )}
-      </Form.Item>
-    </Form>
+
+          <Button
+            type="primary"
+            icon={IS_CREATING ? <EditOutlined /> : <PlusOutlined />}
+            htmlType="submit"
+            onClick={onFinish}
+            style={{ float: 'right', marginRight: 5 }}
+            disabled={IS_CREATING ? !auth?.canCreate : !auth?.canUpdate}
+          >
+            {IS_CREATING ? t('operations.create') : t('operations.update')}
+          </Button>
+
+          {!IS_CREATING && (
+            <Button
+              type="danger"
+              icon={<DeleteOutlined />}
+              style={{ float: 'right', marginRight: 5 }}
+              onClick={onDelete}
+              disabled={!auth?.canDelete}
+            >
+              {t('operations.delete')}
+            </Button>
+          )}
+          )}
+        </>
+      }
+    >
+      <Form layout="vertical" form={form} onFinish={onFinish} scrollToFirstError>
+        <Tabs defaultActiveKey="1">
+          <TabPane tab={t('tabColumns.general')} key="1">
+            <Fields form={form} value={value} />
+          </TabPane>
+        </Tabs>
+      </Form>
+    </Drawer>
   );
 };
 
