05f00aaab52141315e784de9a6a2f1bfe46b748f
WeiCaoMelbourne <weicao.melbourne@gmail.com>
2020-06-15 01:32:49 +1000

    OM5K-6500 The Depot selection is missing on the Inventory Requests screen. (ignore and close the defect if the Depot selection is not required on the html5 version anymore.

--- client/src/pages/inventory-requests/forms/forms.jsx
+++ client/src/pages/inventory-requests/forms/forms.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import {
   EditOutlined,
@@ -8,10 +8,11 @@ import {
   QuestionCircleOutlined,
 } from '@ant-design/icons';
 
-import { Form, Button, Tabs, Modal, notification } from 'antd';
+import { Form, Button, Tabs, Modal, notification, Drawer } from 'antd';
 import { useTranslation } from 'react-i18next';
 import { mutate } from 'swr';
 import axios from 'axios';
+import _ from 'lodash';
 
 import { Type, Period, InventoryDate, SelectAllTanks } from './fields';
 import { INVENTORY_REQUESTS } from '../../../api';
@@ -19,13 +20,21 @@ import { SETTINGS } from '../../../constants';
 
 const TabPane = Tabs.TabPane;
 
-const FormModal = ({ value }) => {
+const FormModal = ({ value, visible, handleFormState, auth }) => {
   const { t } = useTranslation();
   const [form] = Form.useForm();
+  const { resetFields } = form;
 
   const IS_CREATING = !value;
 
-  const onFinish = (values) => {
+  const onComplete = () => {
+    handleFormState(false, null); 
+    mutate(INVENTORY_REQUESTS.READ);
+  };
+
+  const onFinish = async () => {
+    const values = await form.validateFields();
+    
     values.tkrq_due = values?.tkrq_due?.format(SETTINGS.DATE_TIME_FORMAT);
 
     Modal.confirm({
@@ -40,19 +49,20 @@ const FormModal = ({ value }) => {
           .post(IS_CREATING ? INVENTORY_REQUESTS.CREATE : INVENTORY_REQUESTS.UPDATE, values)
           .then(
             axios.spread((response) => {
-              Modal.destroyAll();
+              onComplete();
 
-              mutate(INVENTORY_REQUESTS.READ);
               notification.success({
                 message: IS_CREATING ? t('messages.createSuccess') : t('messages.updateSuccess'),
                 description: IS_CREATING ? t('descriptions.createSuccess') : t('messages.updateSuccess'),
               });
             })
           )
-          .catch((error) => {
-            notification.error({
-              message: error.message,
-              description: IS_CREATING ? t('descriptions.createFailed') : t('descriptions.updateFailed'),
+          .catch((errors) => {
+            _.forEach(errors.response.data.errors, (error) => {
+              notification.error({
+                message: error.type,
+                description: error.message,
+              });
             });
           });
       },
@@ -71,66 +81,89 @@ const FormModal = ({ value }) => {
           .post(INVENTORY_REQUESTS.DELETE, value)
           .then(
             axios.spread((response) => {
-              mutate(INVENTORY_REQUESTS.READ);
-              Modal.destroyAll();
+              onComplete();
+
               notification.success({
                 message: t('messages.deleteSuccess'),
                 description: `${t('descriptions.deleteSuccess')}`,
               });
             })
           )
-          .catch((error) => {
-            notification.error({
-              message: error.message,
-              description: t('descriptions.deleteFailed'),
+          .catch((errors) => {
+            _.forEach(errors.response.data.errors, (error) => {
+              notification.error({
+                message: error.type,
+                description: error.message,
+              });
             });
           });
       },
     });
   };
 
+  useEffect(() => {
+    if (!value && !visible) {
+      resetFields();
+    } 
+  }, [value, visible]);
+
   return (
-    <Form scro layout="vertical" form={form} onFinish={onFinish} scrollToFirstError>
-      <Tabs defaultActiveKey="1" style={{ height: '40vh' }}>
-        <TabPane tab={t('tabColumns.general')} key="1">
-          <Type form={form} value={value} />
-          <Period form={form} value={value} />
-          <InventoryDate form={form} value={value} />
-          <SelectAllTanks form={form} value={value} />
-        </TabPane>
-      </Tabs>
-
-      <Form.Item>
-        <Button
-          htmlType="button"
-          icon={<CloseOutlined />}
-          style={{ float: 'right' }}
-          onClick={() => Modal.destroyAll()}
-        >
-          {t('operations.cancel')}
-        </Button>
-
-        <Button
-          type="primary"
-          icon={IS_CREATING ? <EditOutlined /> : <PlusOutlined />}
-          htmlType="submit"
-          style={{ float: 'right', marginRight: 5 }}
-        >
-          {IS_CREATING ? t('operations.create') : t('operations.update')}
-        </Button>
-
-        {!IS_CREATING && (
+    <Drawer
+      bodyStyle={{ paddingTop: 5 }}
+      onClose={() => handleFormState(false, null)}
+      maskClosable={IS_CREATING}
+      destroyOnClose={true}
+      mask={IS_CREATING}
+      placement="right"
+      width="50vw"
+      visible={visible}
+      footer={
+        <>
           <Button
-            type="danger"
-            icon={<DeleteOutlined />}
-            style={{ float: 'right', marginRight: 5 }}
-            onClick={onDelete}
+            htmlType="button"
+            icon={<CloseOutlined />}
+            style={{ float: 'right' }}
+            onClick={() => handleFormState(false, null)}
           >
-            {t('operations.delete')}
+            {t('operations.cancel')}
           </Button>
-        )}
-      </Form.Item>
-    </Form>
+
+          <Button
+            type="primary"
+            icon={IS_CREATING ? <EditOutlined /> : <PlusOutlined />}
+            htmlType="submit"
+            onClick={onFinish}
+            style={{ float: 'right', marginRight: 5 }}
+            disabled={IS_CREATING ? !auth?.canCreate : !auth?.canUpdate}
+          >
+            {IS_CREATING ? t('operations.create') : t('operations.update')}
+          </Button>
+
+          {!IS_CREATING && (
+            <Button
+              type="danger"
+              icon={<DeleteOutlined />}
+              style={{ float: 'right', marginRight: 5 }}
+              onClick={onDelete}
+              disabled={!auth?.canDelete}
+            >
+              {t('operations.delete')}
+            </Button>
+          )}
+        </>
+      }
+    >
+      <Form scro layout="vertical" form={form} onFinish={onFinish} scrollToFirstError>
+        <Tabs defaultActiveKey="1" style={{ height: '40vh' }}>
+          <TabPane tab={t('tabColumns.general')} key="1">
+            <Type form={form} value={value} />
+            <Period form={form} value={value} />
+            <InventoryDate form={form} value={value} />
+            <SelectAllTanks form={form} value={value} />
+          </TabPane>
+        </Tabs>
+      </Form>
+    </Drawer>
   );
 };
 
