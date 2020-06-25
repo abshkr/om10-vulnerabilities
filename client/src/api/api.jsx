188bd9f5eba22be65f51072efe1324a35141e2f6
Mash Wimalawardana <mashdroidd@gmail.com>
2020-06-25 19:11:17 +1000

    Addded Deconstructed Token

--- client/src/api/api.jsx
+++ client/src/api/api.jsx
@@ -1,12 +1,13 @@
 import Axios from 'axios';
 import authStore from '../stores/auth-store';
 
-console.log(authStore.getState());
+const token = authStore.getState()?.auth?.authenticated;
 
 const api = Axios.create({
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
+    Authorization: token,
   },
 });
 
