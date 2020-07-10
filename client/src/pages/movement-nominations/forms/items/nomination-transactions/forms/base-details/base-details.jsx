8b167ad32a8e08f74cfb413028814843da581a2b
Bin Zhou <binzhou@diamondkey.com>
2020-07-11 07:42:36 +0800

    OM5K-7079

--- client/src/pages/movement-nominations/forms/items/nomination-transactions/forms/base-details/base-details.jsx
+++ client/src/pages/movement-nominations/forms/items/nomination-transactions/forms/base-details/base-details.jsx
@@ -1,28 +1,42 @@
 import React from 'react';
+import {Row, Col} from 'antd';
 
 import { useTranslation } from 'react-i18next';
 
 import { DataTable } from '../../../../../../../components';
 import columns from './columns';
+import { useState } from 'react';
+import _ from 'lodash';
 
 const BaseDetails = ({ form, value, pageState, arm }) => {
   const { t } = useTranslation();
+  const [data, setData] = useState([]);
 
   //const fields = columns(t);
 
   return (
     <>
       <DataTable
+        minimal={true}
         data={[]}
         height="80vh"
         columns={columns(t, pageState, form, arm)}
         //columns={fields}
       />
-      <div style={{ display: 'flex', justifyContent: 'center', marginTop: 10 }}>
-        <div style={{ marginRight: 20 }}>
-          <strong>Base Obsered Total: {500}</strong>
-        </div>
-      </div>
+      <Row gutter={[8,8]}>
+        <Col span={9}>
+        </Col>
+        <Col span={5}>
+          <strong>{t('fields.nomtranObsTotal')} {_.round(_.sumBy(data, 'trsf_bs_qty_amb'), 3)}</strong>
+        </Col>
+        <Col span={5}>
+          <strong>{t('fields.nomtranStdTotal')} {_.round(_.sumBy(data, 'trsf_bs_qty_cor'), 3)}</strong>
+        </Col>
+        <Col span={5}>
+          <strong>{t('fields.nomtranMassTotal')} {_.round(_.sumBy(data, 'trsf_bs_load_kg'), 3)}</strong>
+        </Col>
+      </Row>
+
     </>
   );
 };
