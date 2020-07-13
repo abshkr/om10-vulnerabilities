c707df8845e898e7d741a2e8d702253f888a25b5
Bin Zhou <binzhou@diamondkey.com>
2020-07-13 16:27:11 +0800

    Weight in Air = tank_vapour_kg; Weight in Vacuum = tank_liquid_kg;  Water Level = tank_water_lvl; Maximum Capacity = tank_max_capacity (It was wrong to make it equal to tank_ullage plus tank_cor_vol)

--- client/src/pages/tanks/overview/overview.jsx
+++ client/src/pages/tanks/overview/overview.jsx
@@ -41,33 +41,36 @@ const Overview = ({ selected, isLoading }) => {
               </Descriptions.Item>
 
               <Descriptions.Item label={t('fields.observedVolume')} span={24}>
-                {selected?.tank_amb_vol} Litres
+                {selected?.tank_amb_vol} {t('units.litres')}
               </Descriptions.Item>
 
               <Descriptions.Item label={t('fields.standardVolume')} span={24}>
-                {selected?.tank_cor_vol} Litres
+                {selected?.tank_cor_vol} {t('units.litres')}
               </Descriptions.Item>
 
               <Descriptions.Item label={t('fields.temperature')} span={24}>
-                {selected?.tank_temp} C
+                {selected?.tank_temp} {t('units.degC')}
               </Descriptions.Item>
 
               <Descriptions.Item label={t('fields.standardDensity')} span={24}>
-                {selected?.tank_15_density} Kg/M3
+                {selected?.tank_15_density} {t('units.kg/m3')}
               </Descriptions.Item>
 
               <Descriptions.Item label={t('fields.weightInAir')} span={24}>
                 {selected?.tank_vapour_kg} Kg
               </Descriptions.Item>
 
-              <Descriptions.Item label={t('fields.weightInVacuum')} span={24}></Descriptions.Item>
+              <Descriptions.Item label={t('fields.weightInVacuum')} span={24}>
+                {selected?.tank_liquid_kg} Kg
+              </Descriptions.Item>
 
               <Descriptions.Item label={t('fields.waterLevel')} span={24}>
-                {selected?.tank_water_lvl} Kg
+                {selected?.tank_water_lvl} mm
               </Descriptions.Item>
 
               <Descriptions.Item label={t('fields.maximumCapacity')} span={24}>
-                {_.toNumber(selected?.tank_ullage) || 0 + _.toNumber(selected?.tank_cor_vol) || 0} Litres
+                {selected?.tank_max_capacity} {selected?.tank_max_capacity==='' ? '' : t('units.litres')}
+                {/* {_.toNumber(selected?.tank_ullage) || 0 + _.toNumber(selected?.tank_cor_vol) || 0} Litres */}
               </Descriptions.Item>
 
               <Descriptions.Item label={t('fields.classification')} span={24}>
