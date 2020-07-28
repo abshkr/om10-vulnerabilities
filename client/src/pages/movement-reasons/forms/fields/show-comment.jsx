9fa9c7daf6eb76c3b4d1fe0b764fc4457464ae54
shl <sweehinglee@diamondkey.com>
2020-07-28 10:51:14 +0000

    OM5K-7223: Add a new column to the Movement Reasons screen with the heading Show Comment

---
+++ client/src/pages/movement-reasons/forms/fields/show-comment.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Form, Checkbox } from 'antd';
+
+const ShowComment = ({ form, value, onChange }) => {
+  const { setFieldsValue } = form;
+
+  const { t } = useTranslation();
+
+  const handleCheck = e => {
+    onChange(e.target.checked);
+  };
+
+  useEffect(() => {
+    if (value) {
+      onChange(value.mr_show_comment);
+
+      setFieldsValue({
+        mr_show_comment: value.mr_show_comment
+      });
+    }
+  }, [value, onChange, setFieldsValue]);
+
+  return (
+    <Form.Item name="mr_show_comment" valuePropName="checked">
+      <Checkbox onChange={handleCheck}>{t('fields.showComment')}</Checkbox>
+    </Form.Item>
+  );
+};
+
+export default ShowComment;
