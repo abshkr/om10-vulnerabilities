c5089b399ac6a21cace0896a8735bd6f36143230
Bin Zhou <binzhou@diamondkey.com>
2020-08-12 08:43:05 +0800

    OM5K-7360 add validation on alphanumeric and length (40)

--- client/src/pages/load-schedules/forms/fields/host-data.jsx
+++ client/src/pages/load-schedules/forms/fields/host-data.jsx
@@ -2,12 +2,30 @@ import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Form, Input } from 'antd';
 
+import { REGEX } from '../../../../constants';
+
 const HostData = ({ form, value }) => {
   const { t } = useTranslation();
 
   const { setFieldsValue } = form;
 
   const IS_DISABLED = !value ? false : value?.shls_status !== 'NEW SCHEDULE';
+
+  const validate = (rule, input) => {
+    const regex = new RegExp(REGEX.ALPHANUMERIC);
+    const validated = regex.exec(input);
+
+    if (input && input.length > 0 && !validated) {
+      return Promise.reject(`${t('validate.invalidInput')}: ${t('descriptions.mustBeAlphaNumeric')}`);
+    }
+
+    const len = (new TextEncoder().encode(input)).length;
+    if (input && input.length > 40) {
+      return Promise.reject(`${t('placeholder.maxCharacters')}: 40 ─ ${t('descriptions.maxCharacters')}`);
+    }
+
+    return Promise.resolve();
+  };
 
   useEffect(() => {
     if (value) {
@@ -18,7 +36,7 @@ const HostData = ({ form, value }) => {
   }, [value, setFieldsValue]);
 
   return (
-    <Form.Item name="shl_fleet_data" label={t('fields.hostData')}>
+    <Form.Item name="shl_fleet_data" label={t('fields.hostData')} rules={[{ required: false, validator: validate }]}>
       <Input style={{ width: '100%' }} maxLength={40} disabled={IS_DISABLED} />
     </Form.Item>
   );
