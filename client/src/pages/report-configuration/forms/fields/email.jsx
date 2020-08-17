c69053a1a95fc899024adec79cb5d47803411869
WeiCaoMelbourne <weicao.melbourne@gmail.com>
2020-08-17 17:51:25 +1000

    OM5K-7088 Report Configuration - create issue - special character in email may not checked?

--- client/src/pages/report-configuration/forms/fields/email.jsx
+++ client/src/pages/report-configuration/forms/fields/email.jsx
@@ -8,10 +8,20 @@ const Email = ({ form, value, enabled, canEmail }) => {
   const { setFieldsValue } = form;
 
   const validate = (rule, input) => {
+    const temp_str = input.replace(":", ";");
+    const email_array = temp_str.split(";");
     // eslint-disable-next-line
     const regex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
-    if (input && !regex.exec(input) && canEmail) {
+    let invalid_found = false;
+    for (let i = 0; i < email_array.length; i++) {
+      if (email_array[i] && !regex.exec(email_array[i]) && canEmail) {
+        invalid_found = true;
+        break;
+      }
+    }
+
+    if (invalid_found && canEmail) {
       return Promise.reject(`${t('placeholder.incorrectFormat')} ─ ${t('placeholder.emailInvalid')}`);
     }
 
