b38805c67ad2e205a8492fb3f3593ba681bc89b5
Scott Burgess <scottburgess@diamondkey.com>
2020-08-19 17:07:40 +1000

    Setting version to 10.1.7-rc.2

--- client/src/constants/settings.jsx
+++ client/src/constants/settings.jsx
@@ -1,4 +1,4 @@
 export const IS_DEVELOPMENT = process.env.NODE_ENV === 'development';
 export const DATE_TIME_FORMAT = 'YYYY-MM-DD HH:mm:ss';
 export const DEFAULT_LANGUAGE = 'en';
-export const VERSION = '10.1.6';
+export const VERSION = '10.1.7-rc.2';
