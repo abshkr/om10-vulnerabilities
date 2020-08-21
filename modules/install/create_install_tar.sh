71117d52cec4fc1b5a0d43184489e87491d256f9
Scott Burgess <scottburgess@diamondkey.com>
2020-08-21 17:29:03 +1000

    Fixed installation of some js for load_bays screen

--- modules/install/create_install_tar.sh
+++ modules/install/create_install_tar.sh
@@ -7,8 +7,8 @@ OM_VER=`git describe --dirty --always --tags`
 mkdir -p tmp/$OM_VER/frontend/
 # Copy required files to frontend/
 cp -r client/build/* server/api/ server/amfservices/ server/phpwrapper/ modules/load_bays/en/ modules/load_bays/images/ tmp/$OM_VER/frontend/
-# Are these js files are required to be in /var/www/htdocs/? Or frontend?
-# cp modules/load_bays/*.js tmp/$OM_VER/
+cp modules/load_bays/*.js tmp/$OM_VER/frontend/
+
 cd tmp/
 tar --create --gzip --verbose --owner=omega --group=omega --file $OM_VER.tar.gz $OM_VER/
 mv $OM_VER.tar.gz ..
