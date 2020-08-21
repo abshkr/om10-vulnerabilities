519c1e7cbc64a69ac23a891ebc7271e6575ce3a9
Bin Zhou <binzhou@diamondkey.com>
2020-08-21 18:17:23 +0800

    add cell validation to delivery details items

--- client/src/pages/delivery-details/forms/dd-items/delivery-detail-items.jsx
+++ client/src/pages/delivery-details/forms/dd-items/delivery-detail-items.jsx
@@ -8,7 +8,7 @@ import {
   LockOutlined,
 } from '@ant-design/icons';
 import { useTranslation } from 'react-i18next';
-import { Button, Form, Select, Drawer, Tooltip } from 'antd';
+import { Button, Form, Select, Drawer, Tooltip, notification } from 'antd';
 import useSWR from 'swr';
 import _ from 'lodash';
 
@@ -16,6 +16,7 @@ import { DataTable } from '../../../../components';
 import { DELIVERY_DETAILS } from '../../../../api';
 
 import columns from './columns';
+import cells from './cells';
 import DdiAdditionalInfo from '../ddi-addl-info';
 
 const DeliveryDetailItems = ({
@@ -32,195 +33,6 @@ const DeliveryDetailItems = ({
   tableAPI,
   setTableAPI,
 }) => {
-  const lineFields = [
-    {
-      code: 'ddi_dd_number',
-      label: t('fields.ddiDdNumber'),
-      dataType: 'STRING',
-      maxLength: 40,
-      editable: false,
-      required: true,
-      precision: null,
-      min: null,
-      max: null,
-    }, 
-    {
-      code: 'ddi_dd_supp_code',
-      label: t('fields.ddiDdSuppName'),
-      dataType: 'STRING',
-      maxLength: 16,
-      editable: false,
-      required: true,
-      precision: null,
-      min: null,
-      max: null,
-    }, 
-    {
-      code: 'ddi_dd_tripord_no',
-      label: t('fields.ddiDdTripOrdNo'),
-      dataType: 'NUMBER',
-      maxLength: 9,
-      editable: false,
-      required: true,
-      precision: 0,
-      min: 0,
-      max: 999999999,
-    }, 
-    {
-      code: 'ddi_dd_ld_type',
-      label: t('fields.ddiDdLoadTypeName'),
-      dataType: 'NUMBER',
-      maxLength: 2,
-      editable: false,
-      required: true,
-      precision: 0,
-      min: 0,
-      max: 99,
-    }, 
-    {
-      code: 'ddi_line_item_num',
-      label: t('fields.ddiLineItemNum'),
-      dataType: 'NUMBER',
-      maxLength: 6,
-      editable: false,
-      required: true,
-      precision: 0,
-      min: 0,
-      max: 999999,
-    }, 
-    {
-      code: 'ddi_item_cat',
-      label: t('fields.ddiItemCat'),
-      dataType: 'NUMBER',
-      maxLength: 4,
-      editable: true,
-      required: false,
-      precision: 0,
-      min: 0,
-      max: 9999,
-    }, 
-    {
-      code: 'ddi_cmpt_num',
-      label: t('fields.ddiCmptNum'),
-      dataType: 'NUMBER',
-      maxLength: 2,
-      editable: true,
-      required: false,
-      precision: 0,
-      min: 0,
-      max: 99,
-    }, 
-    {
-      code: 'ddi_prod_code',
-      label: t('fields.ddiProdCode'),
-      dataType: 'STRING',
-      maxLength: 36,
-      editable: false,
-      required: true,
-      precision: null,
-      min: null,
-      max: null,
-    }, 
-    {
-      code: 'ddi_cmpy_code',
-      label: t('fields.ddiCmpyCode'),
-      dataType: 'STRING',
-      maxLength: 16,
-      editable: false,
-      required: true,
-      precision: null,
-      min: null,
-      max: null,
-    }, 
-    {
-      code: 'ddi_qty',
-      label: t('fields.ddiQty'),
-      dataType: 'NUMBER',
-      maxLength: 9,
-      editable: true,
-      required: true,
-      precision: 0,
-      min: 0,
-      max: 999999999,
-    }, 
-    {
-      code: 'ddi_unit',
-      label: t('fields.ddiUnit'),
-      dataType: 'NUMBER',
-      maxLength: 2,
-      editable: false,
-      required: true,
-      precision: 0,
-      min: 0,
-      max: 99,
-    }, 
-    {
-      code: 'ddi_item_desc',
-      label: t('fields.ddiItemDesc'),
-      dataType: 'STRING',
-      maxLength: 300,
-      editable: true,
-      required: false,
-      precision: null,
-      min: null,
-      max: null,
-    }, 
-    {
-      code: 'ddi_duty_code',
-      label: t('fields.ddiDutyCode'),
-      dataType: 'STRING',
-      maxLength: 20,
-      editable: true,
-      required: false,
-      precision: null,
-      min: null,
-      max: null,
-    }, 
-    {
-      code: 'ddi_excise_lic_num',
-      label: t('fields.ddiExciseLicNum'),
-      dataType: 'NUMBER',
-      maxLength: 15,
-      editable: true,
-      required: false,
-      precision: 0,
-      min: 0,
-      max: 999999999999999,
-    }, 
-    {
-      code: 'ddi_ref_doc_num',
-      label: t('fields.ddiRefDocNum'),
-      dataType: 'NUMBER',
-      maxLength: 10,
-      editable: true,
-      required: false,
-      precision: 0,
-      min: 0,
-      max: 9999999999,
-    }, 
-    {
-      code: 'ddi_site_capacity',
-      label: t('fields.ddiSiteCapacity'),
-      dataType: 'NUMBER',
-      maxLength: 10,
-      editable: true,
-      required: false,
-      precision: 0,
-      min: 0,
-      max: 9999999999,
-    }, 
-    {
-      code: 'ddi_tank_code',
-      label: t('fields.ddiTankCode'),
-      dataType: 'STRING',
-      maxLength: 20,
-      editable: true,
-      required: false,
-      precision: null,
-      min: null,
-      max: null,
-    }, 
-  ];
   const [selected, setSelected] = useState(null);
   // const [tableAPI, setTableAPI] = useState(null);
   const [size, setSize] = useState(0);
@@ -316,10 +128,108 @@ const DeliveryDetailItems = ({
     setSelected(items);
   };
 
+  const onCellValidation = (input, line, rule) => {
+    /*
+      rule:
+      {
+        code: 'ddi_dd_number',
+        label: t('fields.ddiDdNumber'),
+        dataType: 'STRING',
+        maxLength: 40,
+        editable: false,
+        required: true,
+        precision: null,
+        min: null,
+        max: null,
+      }
+    */
+    const errors = [];
+
+    if ((rule.required && input === '') || (rule.required && !input)) {
+      errors.push({
+        key: String(line) + ':' + rule.code + '_required',
+        field: rule.label + ' [' + t('fields.line') + ' ' + line + ']',
+        message: `${t('validate.set')} ─ ${rule.label}`
+      });
+    }
+
+    const len = (new TextEncoder().encode(input)).length;
+    if ((rule.maxLength != undefined && rule.maxLength != null) && input && len > rule.maxLength) {
+      errors.push({
+        key: String(line) + ':' + rule.code + '_maxlen',
+        field: rule.label + ' [' + t('fields.line') + ' ' + line + ']',
+        message: `${t('placeholder.maxCharacters')}: ${rule.maxLength} ─ ${t('descriptions.maxCharacters')}`
+      });
+    }
+
+    if (rule.dataType === 'STRING') {
+
+    }
+
+    if (rule.dataType === 'NUMBER') {
+      const number = _.toNumber(input);
+      const invalid = _.isNaN(number);
+  
+      const decimals = _.toString(number).split('.')[1]?.length || 0;
+      
+      if (input && input !== '' && invalid) {
+        errors.push({
+          key: String(line) + ':' + rule.code + '_invalid',
+          field: rule.label + ' [' + t('fields.line') + ' ' + line + ']',
+          message: `${t('validate.wrongType')}: ${t('validate.mustBeNumber')}`
+        });
+      }
+  
+      if (rule.precision !== undefined && rule.precision !== null && decimals > rule.precision) {
+        errors.push({
+          key: String(line) + ':' + rule.code + '_precision',
+          field: rule.label + ' [' + t('fields.line') + ' ' + line + ']',
+          message: `${t('validate.decimalPlacesExceeded')} ${rule.precision} ─ ${t('descriptions.invalidDecimals')}`
+        });
+      }
+  
+      if (rule.max !== undefined && rule.max !== null && input !== '' && !invalid && number > rule.max) {
+        errors.push({
+          key: String(line) + ':' + rule.code + '_maximum',
+          field: rule.label + ' [' + t('fields.line') + ' ' + line + ']',
+          message: `${t('validate.outOfRangeMax')} ${rule.max} ─ ${t('descriptions.maxNumber')}`
+        });
+      }
+  
+      if (rule.min !== undefined && rule.min !== null && input !== '' && !invalid && number < rule.min) {
+        errors.push({
+          key: String(line) + ':' + rule.code + '_minimum',
+          field: rule.label + ' [' + t('fields.line') + ' ' + line + ']',
+          message: `${t('validate.outOfRangeMin')} ${rule.min} ─ ${t('descriptions.minNumber')}`
+        });
+      }
+  
+    }
+
+    _.forEach(errors, error => {
+      notification.error({
+        message: error.field,
+        description: error.message,
+        key: error.key
+      });
+    });
+
+    return errors;
+  };
+
   const onEditingFinished = (value) => {
     let payload = value.data;
 
     console.log('onEditingFinished', value, value.colDef);
+
+    const rule = _.find(cells(t), (o) => (o.code ===value.colDef.field));
+    console.log('onEditingFinished, rule', rule);
+
+    const errors = onCellValidation(payload[value.colDef.field], payload?.ddi_line_item_num, rule);
+
+    if (errors.length > 0) {
+      payload[value.colDef.field] = '';
+    }
 
     tableAPI.updateRowData({ update: [payload] });
 
