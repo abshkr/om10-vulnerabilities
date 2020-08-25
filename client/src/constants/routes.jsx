ce86e52ce21510236807032cfbb79530bf55c9a5
WeiCaoMelbourne <weicao.melbourne@gmail.com>
2020-08-25 22:23:43 +1000

    Added

--- client/src/constants/routes.jsx
+++ client/src/constants/routes.jsx
@@ -3,6 +3,7 @@ export const ROOT = '/';
 
 export const HOME = '/home';
 export const UNAUTHORIZED = '/unauthorized';
+export const NOT_FOUND = '/not-found';
 export const LOG_IN = '/login';
 export const LOG_OUT = '/logout';
 export const _404 = '/404';
