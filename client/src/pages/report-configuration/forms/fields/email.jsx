b46b792928f3c8a9702cd177e097de274e52be32
Mash Wimalawardana <mashdroidd@gmail.com>
2020-10-09 16:08:47 +1100

    OM5K-7987 - Adding new report config force user to key in email address
    Error was caused by the input.replace throwing an error as input was undefined due to no user input thus the replace properly was missing which meant the email_array split also returned an error failing the loop. The entire function was wrapped in a promise so the entire function threw a failed state causing the odd Error State for that form.

--- client/src/pages/report-configuration/forms/fields/email.jsx
+++ client/src/pages/report-configuration/forms/fields/email.jsx
@@ -7,13 +7,24 @@ const Email = ({ form, value, enabled, canEmail }) => {
 
   const { setFieldsValue } = form;
 
+
+
   const validate = (rule, input) => {
-    const temp_str = input.replace(":", ";");
-    const email_array = temp_str.split(";");
+
+    /*
+      Please do not directly manipulate lets as we try to follow an immutable data flow
+      manipulating lets will cause rendering issues and mismatched states
+      also because this function was implemented in a promise. all errors to do with the function would be returned as the promise failed.
+    */
+
+
+    const temp_str = input?.replace(":", ";"); 
+    const email_array = temp_str?.split(";") || []
     // eslint-disable-next-line
     const regex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
     let invalid_found = false;
+
     for (let i = 0; i < email_array.length; i++) {
       if (email_array[i] && !regex.exec(email_array[i]) && canEmail) {
         invalid_found = true;
