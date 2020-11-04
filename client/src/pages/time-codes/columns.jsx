b8b38fa6982b599978b9218c3fae03e664ab48a5
Mash Wimalawardana <mashdroidd@gmail.com>
2020-11-05 09:29:45 +1100

    OM5K-8177 - Sorting Day of Time code is not sorted in the order.
    Added a Comparator.

--- client/src/pages/time-codes/columns.jsx
+++ client/src/pages/time-codes/columns.jsx
@@ -1,4 +1,21 @@
-const columns = t => [
+function dayOfWeekSort(prevDay, nextDay) {
+  const sorter = {
+    monday: 1,
+    tuesday: 2,
+    wednesday: 3,
+    thursday: 4,
+    friday: 5,
+    saturday: 6,
+    sunday: 7,
+  };
+
+  let day1 = prevDay?.toLowerCase();
+  let day2 = nextDay?.toLowerCase();
+
+  return sorter[day1] - sorter[day2];
+}
+
+const columns = (t) => [
   {
     headerName: t('fields.day'),
     field: 'day',
@@ -9,7 +26,8 @@ const columns = t => [
     pinned: 'left',
     headerCheckboxSelection: true,
     headerCheckboxSelectionFilteredOnly: true,
-    checkboxSelection: true
+    checkboxSelection: true,
+    comparator: dayOfWeekSort,
   },
 
   {
@@ -18,169 +36,169 @@ const columns = t => [
     filter: 'BooleanFilter',
     sortable: true,
     resizable: true,
-    cellRenderer: 'BooleanRenderer'
+    cellRenderer: 'BooleanRenderer',
   },
   {
     headerName: '01:00',
     field: '1',
     sortable: true,
     resizable: true,
-    cellRenderer: 'BooleanRenderer'
+    cellRenderer: 'BooleanRenderer',
   },
   {
     headerName: '02:00',
     field: '2',
     sortable: true,
     resizable: true,
-    cellRenderer: 'BooleanRenderer'
+    cellRenderer: 'BooleanRenderer',
   },
   {
     headerName: '03:00',
     field: '3',
     sortable: true,
     resizable: true,
-    cellRenderer: 'BooleanRenderer'
+    cellRenderer: 'BooleanRenderer',
   },
   {
     headerName: '04:00',
     field: '4',
     sortable: true,
     resizable: true,
-    cellRenderer: 'BooleanRenderer'
+    cellRenderer: 'BooleanRenderer',
   },
   {
     headerName: '05:00',
     field: '5',
     sortable: true,
     resizable: true,
-    cellRenderer: 'BooleanRenderer'
+    cellRenderer: 'BooleanRenderer',
   },
   {
     headerName: '06:00',
     field: '6',
     sortable: true,
     resizable: true,
-    cellRenderer: 'BooleanRenderer'
+    cellRenderer: 'BooleanRenderer',
   },
   {
     headerName: '07:00',
     field: '7',
     sortable: true,
     resizable: true,
-    cellRenderer: 'BooleanRenderer'
+    cellRenderer: 'BooleanRenderer',
   },
   {
     headerName: '08:00',
     field: '8',
     sortable: true,
     resizable: true,
-    cellRenderer: 'BooleanRenderer'
+    cellRenderer: 'BooleanRenderer',
   },
   {
     headerName: '09:00',
     field: '9',
     sortable: true,
     resizable: true,
-    cellRenderer: 'BooleanRenderer'
+    cellRenderer: 'BooleanRenderer',
   },
   {
     headerName: '10:00',
     field: '10',
     sortable: true,
     resizable: true,
-    cellRenderer: 'BooleanRenderer'
+    cellRenderer: 'BooleanRenderer',
   },
   {
     headerName: '11:00',
     field: '11',
     sortable: true,
     resizable: true,
-    cellRenderer: 'BooleanRenderer'
+    cellRenderer: 'BooleanRenderer',
   },
   {
     headerName: '12:00',
     field: '12',
     sortable: true,
     resizable: true,
-    cellRenderer: 'BooleanRenderer'
+    cellRenderer: 'BooleanRenderer',
   },
   {
     headerName: '13:00',
     field: '13',
     sortable: true,
     resizable: true,
-    cellRenderer: 'BooleanRenderer'
+    cellRenderer: 'BooleanRenderer',
   },
   {
     headerName: '14:00',
     field: '14',
     sortable: true,
     resizable: true,
-    cellRenderer: 'BooleanRenderer'
+    cellRenderer: 'BooleanRenderer',
   },
   {
     headerName: '15:00',
     field: '15',
     sortable: true,
     resizable: true,
-    cellRenderer: 'BooleanRenderer'
+    cellRenderer: 'BooleanRenderer',
   },
   {
     headerName: '16:00',
     field: '16',
     sortable: true,
     resizable: true,
-    cellRenderer: 'BooleanRenderer'
+    cellRenderer: 'BooleanRenderer',
   },
   {
     headerName: '17:00',
     field: '17',
     sortable: true,
     resizable: true,
-    cellRenderer: 'BooleanRenderer'
+    cellRenderer: 'BooleanRenderer',
   },
   {
     headerName: '18:00',
     field: '18',
     sortable: true,
     resizable: true,
-    cellRenderer: 'BooleanRenderer'
+    cellRenderer: 'BooleanRenderer',
   },
   {
     headerName: '19:00',
     field: '19',
     sortable: true,
     resizable: true,
-    cellRenderer: 'BooleanRenderer'
+    cellRenderer: 'BooleanRenderer',
   },
   {
     headerName: '20:00',
     field: '20',
     sortable: true,
     resizable: true,
-    cellRenderer: 'BooleanRenderer'
+    cellRenderer: 'BooleanRenderer',
   },
   {
     headerName: '21:00',
     field: '21',
     sortable: true,
     resizable: true,
-    cellRenderer: 'BooleanRenderer'
+    cellRenderer: 'BooleanRenderer',
   },
   {
     headerName: '22:00',
     field: '22',
     sortable: true,
     resizable: true,
-    cellRenderer: 'BooleanRenderer'
+    cellRenderer: 'BooleanRenderer',
   },
   {
     headerName: '23:00',
     field: '23',
     sortable: true,
     resizable: true,
-    cellRenderer: 'BooleanRenderer'
-  }
+    cellRenderer: 'BooleanRenderer',
+  },
 ];
 
 export default columns;
