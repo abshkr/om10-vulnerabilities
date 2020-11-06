57d0206b11b986a646da85d882d1888caf35dc4c
Bin Zhou <binzhou@diamondkey.com>
2020-11-06 19:31:53 +0800

    OM5K-8159 Apply DateTimeRangePicker to Order Listing screen

--- client/src/pages/order-listings/order-listings.jsx
+++ client/src/pages/order-listings/order-listings.jsx
@@ -6,7 +6,7 @@ import { Button, Select } from 'antd';
 import { useTranslation } from 'react-i18next';
 import { SyncOutlined, PlusOutlined, FileSearchOutlined } from '@ant-design/icons';
 
-import { Page, DataTable, Download, Calendar, WindowSearch } from '../../components';
+import { Page, DataTable, Download, DateTimeRangePicker, WindowSearch } from '../../components';
 import api, { ORDER_LISTINGS } from '../../api';
 import { SETTINGS } from '../../constants';
 import { useAuth } from '../../hooks';
@@ -14,23 +14,16 @@ import columns from './columns';
 import auth from '../../auth';
 import Forms from './forms';
 import { useConfig } from '../../hooks';
-import { getDateRangeOffset, getCurrentTime } from '../../utils';
 
-const OrderListings = ({popup, params}) => {
-  const [rangeStart, setRangeStart] = useState(0);
-  const [rangeEnd, setRangeEnd] = useState(0);
-
+const OrderListings = ({ popup, params }) => {
   const config = useConfig();
   const rangeSetting = config.openOrderDateRange;
-  const ranges = getDateRangeOffset(config.openOrderDateRange, '30');
-  //const ranges = getDateRangeOffset(false, '30');
-  //const ranges = getDateRangeOffset("7~~0", '30');
   const filterByExpiry = config.filterOpenOrderByExpiry;
   console.log('filterByExpiry', filterByExpiry);
 
   const [visible, setVisible] = useState(false);
   const [selected, setSelected] = useState(null);
-  const [timeOption, setTimeOption] = useState(filterByExpiry?'ORDER_EXP_TIME':'ORDER_ORD_TIME');
+  const [timeOption, setTimeOption] = useState(filterByExpiry ? 'ORDER_EXP_TIME' : 'ORDER_ORD_TIME');
   const [pageState, setPageState] = useState('view');
   const [supplier, setSupplier] = useState(null);
   const [customer, setCustomer] = useState(null);
@@ -53,21 +46,20 @@ const OrderListings = ({popup, params}) => {
 
   const access = useAuth('M_ORDERLISTING');
 
+  const [refreshed, setRefreshed] = useState(false);
+  // const [start, setStart] = useState(moment().subtract(7, 'days').format(SETTINGS.DATE_TIME_FORMAT));
+  // const [end, setEnd] = useState(moment().add(7, 'days').format(SETTINGS.DATE_TIME_FORMAT));
   const [start, setStart] = useState(null);
   const [end, setEnd] = useState(null);
 
   const url =
     popup && supplier && customer
-      ? (
-        start && end
+      ? start && end
         ? `${ORDER_LISTINGS.READ}?start_date=${start}&end_date=${end}&time_option=${timeOption}&order_supp_code=${supplier}&order_cust_acnt=${customer}`
         : null // `${ORDER_LISTINGS.READ}?order_supp_code=${supplier}&order_cust_acnt=${customer}`
-      )
-      : (
-        start && end
-        ? `${ORDER_LISTINGS.READ}?start_date=${start}&end_date=${end}&time_option=${timeOption}`
-        : null
-      );
+      : start && end
+      ? `${ORDER_LISTINGS.READ}?start_date=${start}&end_date=${end}&time_option=${timeOption}`
+      : null;
 
   const { data: payload, isValidating, revalidate } = useSWR(url, { revalidateOnFocus: false });
 
@@ -101,110 +93,60 @@ const OrderListings = ({popup, params}) => {
   };
 
   const setRange = (start, end) => {
-    // setStart(start);
-    // setEnd(end);
-    if (rangeSetting !== '-1~~-1') {
-      setStart(start);
-      setEnd(end);
-    } else {
-      setStart('-1');
-      setEnd('-1');
-    }
+    setStart(start);
+    setEnd(end);
   };
 
-  const onRefresh = async () => {
-    if (rangeSetting !== '-1~~-1') {
-      const currTime = await getCurrentTime();
-      setStart(moment(currTime, SETTINGS.DATE_TIME_FORMAT).subtract(rangeStart, 'days').format(SETTINGS.DATE_TIME_FORMAT));
-      setEnd(moment(currTime, SETTINGS.DATE_TIME_FORMAT).add(rangeEnd, 'days').format(SETTINGS.DATE_TIME_FORMAT));
-      // setStart(moment().subtract(rangeStart, 'days').format(SETTINGS.DATE_TIME_FORMAT));
-      // setEnd(moment().add(rangeEnd, 'days').format(SETTINGS.DATE_TIME_FORMAT));
-    } else {
-      setStart('-1');
-      setEnd('-1');
-      revalidate();
-    }
+  const onRefresh = () => {
+    setRefreshed(true);
+    // setTimeOption(filterByExpiry?'ORDER_EXP_TIME':'ORDER_ORD_TIME');
+
+    // Don't need revalidate, let useSWR handle itself while parameter changes
+    // revalidate();
   };
 
   const locateOrder = (value) => {
     runSearch({
       order_cust_no: value,
-    })
-  }
+    });
+  };
 
   const runSearch = (values) => {
     if (
-      !values?.order_cust_no && 
-      (!supplier && !values?.order_supp_code) && 
-      (!customer && !values?.order_cust_acnt) &&
+      !values?.order_cust_no &&
+      !supplier &&
+      !values?.order_supp_code &&
+      !customer &&
+      !values?.order_cust_acnt &&
       !values?.order_stat_id &&
       !values?.order_ref_code &&
-      !values.use_date_range) {
+      !values.use_date_range
+    ) {
       return;
     }
 
     api
-    .get(ORDER_LISTINGS.SEARCH, {
-      params: {
-        order_cust_no: values?.order_cust_no,
-        order_supp_code: !supplier ? values?.order_supp_code : supplier,
-        order_cust_acnt: !customer ? values?.order_cust_acnt : customer,
-        order_stat_id: values?.order_stat_id,
-        order_ref_code: values?.order_ref_code,
-        start_date: values.use_date_range ? values.start_date : null,
-        end_date: values.use_date_range ? values.end_date : null,
-        time_option: values.use_date_range ? values.time_option : null,
-      },
-    })
-    .then((res) => {
-      setData(res.data.records);
-    });
+      .get(ORDER_LISTINGS.SEARCH, {
+        params: {
+          order_cust_no: values?.order_cust_no,
+          order_supp_code: !supplier ? values?.order_supp_code : supplier,
+          order_cust_acnt: !customer ? values?.order_cust_acnt : customer,
+          order_stat_id: values?.order_stat_id,
+          order_ref_code: values?.order_ref_code,
+          start_date: values.use_date_range ? values.start_date : null,
+          end_date: values.use_date_range ? values.end_date : null,
+          time_option: values.use_date_range ? values.time_option : null,
+        },
+      })
+      .then((res) => {
+        setData(res.data.records);
+      });
   };
-
-  useEffect(() => {
-    // console.log("I am here: rangeStart, start", start, rangeStart);
-    if (rangeSetting !== '-1~~-1') {
-      let currTime = moment();
-      if (config.serverTime) {
-        currTime = moment(config.serverTime, SETTINGS.DATE_TIME_FORMAT);
-      }
-      setStart(currTime.subtract(rangeStart, 'days').format(SETTINGS.DATE_TIME_FORMAT));
-      // setStart(moment().subtract(rangeStart, 'days').format(SETTINGS.DATE_TIME_FORMAT));
-    } else {
-      setStart('-1');
-    }
-  }, [rangeStart, rangeSetting, config, setStart]);
-
-  useEffect(() => {
-    // console.log("I am here: rangeEnd, end", end, rangeEnd);
-    if (rangeSetting !== '-1~~-1') {
-      let currTime = moment();
-      if (config.serverTime) {
-        currTime = moment(config.serverTime, SETTINGS.DATE_TIME_FORMAT);
-      }
-      setEnd(currTime.add(rangeEnd, 'days').format(SETTINGS.DATE_TIME_FORMAT));
-      // setEnd(moment().add(rangeEnd, 'days').format(SETTINGS.DATE_TIME_FORMAT));
-    } else {
-      setEnd('-1');
-    }
-  }, [rangeEnd, rangeSetting, config, setEnd]);
-
-  useEffect(() => {
-    if (ranges) {
-      if (rangeSetting !== '-1~~-1') {
-        setRangeStart(ranges?.beforeToday);
-        setRangeEnd(ranges?.afterToday);
-      } else {
-        setRangeStart(-1);
-        setRangeEnd(-1);
-      }
-    }
-  }, [ranges, rangeSetting, setRangeStart, setRangeEnd]);
 
   useEffect(() => {
     if (filterByExpiry) {
       setTimeOption('ORDER_EXP_TIME');
-    }else {
+    } else {
       setTimeOption('ORDER_ORD_TIME');
     }
   }, [filterByExpiry, setTimeOption]);
@@ -225,30 +167,35 @@ const OrderListings = ({popup, params}) => {
 
   const modifiers = (
     <>
-      {rangeSetting !== '-1~~-1' && (
-        <div style={{ float: 'left' }}>
-          <Select
-            dropdownMatchSelectWidth={false}
-            defaultValue={filterByExpiry?'ORDER_EXP_TIME':'ORDER_ORD_TIME'}
-            onChange={setTimeOption}
-            optionFilterProp="children"
-            placeholder={null}
-            filterOption={(input, option) =>
-              option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-            }
-          >
-            {timeOptions.map((item, index) => (
-              <Select.Option key={index} value={item.code}>
-                {item.name}
-              </Select.Option>
-            ))}
-          </Select>
-        </div>
-      )}
+      <div style={{ float: 'left' }}>
+        <Select
+          dropdownMatchSelectWidth={false}
+          defaultValue={filterByExpiry ? 'ORDER_EXP_TIME' : 'ORDER_ORD_TIME'}
+          onChange={setTimeOption}
+          optionFilterProp="children"
+          placeholder={null}
+          filterOption={(input, option) =>
+            option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+          }
+        >
+          {timeOptions.map((item, index) => (
+            <Select.Option key={index} value={item.code}>
+              {item.name}
+            </Select.Option>
+          ))}
+        </Select>
+      </div>
 
-      {rangeSetting !== '-1~~-1' && (
-        <Calendar handleChange={setRange} start={start} end={end} max={1000} />
-      )}
+      <DateTimeRangePicker
+        handleChange={setRange}
+        rangeSetting={rangeSetting}
+        refreshed={refreshed}
+        setRefreshed={setRefreshed}
+        disabled={false}
+        enableClear={true}
+        max={1000}
+        // localBased={true}
+      />
 
       <Button icon={<SyncOutlined />} onClick={onRefresh} loading={isLoading}>
         {t('operations.refresh')}
@@ -256,17 +203,19 @@ const OrderListings = ({popup, params}) => {
 
       <Download data={data} isLoading={isLoading} columns={fields} />
 
-      <Button 
+      <Button
         type="primary"
-        icon={<FileSearchOutlined />} 
-        onClick={() => WindowSearch(runSearch, t('operations.search'), {
-          order_supp_code: !supplier, // true,
-          order_cust_acnt: !customer, // true,
-          order_stat_id: true,
-          order_cust_no: true,
-          order_ref_code: true,
-          time_option: "open_order",
-        })}
+        icon={<FileSearchOutlined />}
+        onClick={() =>
+          WindowSearch(runSearch, t('operations.search'), {
+            order_supp_code: !supplier, // true,
+            order_cust_acnt: !customer, // true,
+            order_stat_id: true,
+            order_cust_no: true,
+            order_ref_code: true,
+            time_option: 'open_order',
+          })
+        }
       >
         {t('operations.search')}
       </Button>
