180a6daaf6233ba236ff6b3527f01c8d25c267d0
WeiCaoMelbourne <weicao.melbourne@gmail.com>
2020-12-09 23:33:47 +1100

    Added

--- server/api/session/fa_auth.php
+++ server/api/session/fa_auth.php
@@ -5,14 +5,14 @@ include_once '../shared/header.php';
 // include database and object files
 include_once '../objects/fa_auth.php';
 
-write_log(sprintf("%s() START. ", __FUNCTION__), __FILE__, __LINE__);
+// write_log(sprintf("%s() START. ", __FUNCTION__), __FILE__, __LINE__);
 
 $database = new Database();
 $db = null;
 
 // initialize object
 try {
-    $db = $database->getConnection2('FaAuth', 'post_check');
+    $db = $database->getConnection2('FaAuth', 'post_check');    //No JWT check 
 } catch (UnauthException $e) {
     $error = new EchoSchema(401, response("__NOT_AUTH__", sprintf("Caught exception: %s", $e->getMessage())));
     echo json_encode($error, JSON_PRETTY_PRINT);
