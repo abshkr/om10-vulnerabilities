e97b0bd25190d9c7ad8fc9c32bda9f17ad58b72b
Bin Zhou <BinZhou@diamondkey.com>
2021-08-30 11:00:04 +1000

    OM5K-9505 fix tank_sg issue in release 10.1.16

--- client/src/pages/tanks/transform.jsx
+++ client/src/pages/tanks/transform.jsx
@@ -14,6 +14,9 @@ const transform = (data) => {
   if (data) {
     for (let index = 0; index < data.length; index++) {
       const element = data[index];
+      element.tank_ifc = element.tank_ifc === undefined ? 0 : element.tank_ifc;
+      element.tank_roof_weight = element.tank_roof_weight === undefined ? 0 : element.tank_roof_weight;
+      element.tank_sg = element.tank_sg === undefined ? '' : element.tank_sg;
 
       payload.push({
         ...element,
