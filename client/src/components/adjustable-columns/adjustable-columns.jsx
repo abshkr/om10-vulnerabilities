61c3478f7230445f387d6161185bb665c5622c53
Bin Zhou <binzhou@diamondkey.com>
2023-01-18 12:55:09 +1100

    OM5K-10831 fixed DOM errors for ScrollBar

--- client/src/components/adjustable-columns/adjustable-columns.jsx
+++ client/src/components/adjustable-columns/adjustable-columns.jsx
@@ -57,71 +57,45 @@ const AdjustableColumns = ({ pageColumns, pageModule, columnAPI, columnLoader })
           {t('operations.saveColumns')}
         </Button>
       </div>
-
-      <Scrollbars
-        style={{
-          height: 'calc(100vh - 235px)',
-          width: '25vw',
-          marginTop: 5,
-          padding: 5,
-        }}
-      >
-        {/* <List
-          style={{ height: 'calc(100vh - 300px)', overflowY: 'auto', minHeight: 720 }}
-          itemLayout="horizontal"
-          size="small"
-          dataSource={data}
-          renderItem={(item) => {
-            return (
-              <List.Item style={{ background: 'white', marginBottom: 10, marginRight: 10, borderRadius: 5 }}>
-                <List.Item.Meta
-                  style={{ justifyContent: 'center', alignContent: 'center', alignItems: 'center' }}
-                  avatar={
-                    <Switch
-                      checked={item.column_visible === '1'}
-                      style={{
-                        visibility: item.column_visible === '0' || item.column_visible === '1' ? 'visible' : 'hidden',
-                      }}
-                      checkedChildren={<span>{t('operations.switchOn')}</span>}
-                      unCheckedChildren={<span>{t('operations.switchOff')}</span>}
-                      // onChange={(value) => onChange(item, value ? '1' : '0')}
-                    />
-                  }
-                  // eslint-disable-next-line
-                  title={`${t(item.column_title)}`}
-                />
-                
-              </List.Item>
-            );
+      <div>
+        <Scrollbars
+          style={{
+            height: 'calc(100vh - 235px)',
+            width: '25vw',
+            marginTop: 5,
+            padding: 5,
           }}
-        /> */}
-        <List
-          style={{ height: 'calc(100vh - 300px)', overflowY: 'auto', minHeight: 500 }}
-          itemLayout="horizontal"
-          size="small"
-          dataSource={items}
-          renderItem={(item) => {
-            return (
-              <List.Item style={{ background: 'white', marginBottom: 10, marginRight: 10, borderRadius: 5 }}>
-                <List.Item.Meta
-                  style={{ justifyContent: 'center', alignContent: 'center', alignItems: 'center' }}
-                  avatar={
-                    <Switch
-                      checked={!item?.colDef?.hide}
-                      // style={{ visibility: item.hide === false || item.hide === true ? 'visible' : 'hidden', }}
-                      checkedChildren={<span>{t('operations.switchShow')}</span>}
-                      unCheckedChildren={<span>{t('operations.switchHide')}</span>}
-                      onChange={(value) => onColumnChanged(item, value)}
-                    />
-                  }
-                  // eslint-disable-next-line
-                  title={item?.colDef?.headerName}
-                />
-              </List.Item>
-            );
-          }}
-        />
-      </Scrollbars>
+        >
+          <List
+            style={{ height: 'calc(100vh - 300px)', overflowY: 'auto', minHeight: 500 }}
+            itemLayout="horizontal"
+            size="small"
+            dataSource={items}
+            renderItem={(item) => {
+              return (
+                <List.Item
+                  style={{ background: 'white', marginBottom: 10, marginRight: 10, borderRadius: 5 }}
+                >
+                  <List.Item.Meta
+                    style={{ justifyContent: 'center', alignContent: 'center', alignItems: 'center' }}
+                    avatar={
+                      <Switch
+                        checked={!item?.colDef?.hide}
+                        // style={{ visibility: item.hide === false || item.hide === true ? 'visible' : 'hidden', }}
+                        checkedChildren={<span>{t('operations.switchShow')}</span>}
+                        unCheckedChildren={<span>{t('operations.switchHide')}</span>}
+                        onChange={(value) => onColumnChanged(item, value)}
+                      />
+                    }
+                    // eslint-disable-next-line
+                    title={item?.colDef?.headerName}
+                  />
+                </List.Item>
+              );
+            }}
+          />
+        </Scrollbars>
+      </div>
     </Menu>
   );
 
--- client/src/components/events/events.jsx
+++ client/src/components/events/events.jsx
@@ -14,12 +14,12 @@ const Events = () => {
   const [playing, toggle, muted, setMuted] = useAudio(COMMON.WARNING_SOUND);
   const { refreshAlarm } = useConfig();
 
-  const [lastSequnce, setLastSequence] = useState(null)
+  const [lastSequnce, setLastSequence] = useState(null);
   const [alarms, setAlarms] = useState(JSON.parse(sessionStorage.getItem('alarms')) || []);
-  
+
   const [visible, setVisible] = useState(false);
   const [seen, setSeen] = useState([]);
-  
+
   const onClearAll = () => {
     const unique = [...new Set(alarms.map((item) => `${item?.gen_date}-${item?.message}`))];
 
@@ -60,11 +60,11 @@ const Events = () => {
 
   const storageSyncTabs = (event) => {
     if (!event) {
-      event = window.event; 
+      event = window.event;
     }
 
-    if(!event.newValue) {
-      return;          // do nothing if no value to work with
+    if (!event.newValue) {
+      return; // do nothing if no value to work with
     }
 
     //Current tab is not active, and other tabs changed alarms
@@ -74,45 +74,51 @@ const Events = () => {
   };
 
   useEffect(() => {
-    window.addEventListener("storage", storageSyncTabs, false);
-  }, [])
+    window.addEventListener('storage', storageSyncTabs, false);
+  }, []);
 
-  const retrieveAlarms = () =>  {
-    api.get(AUTH.SESSION, {
+  const retrieveAlarms = () => {
+    api
+      .get(AUTH.SESSION, {
         params: {
           prev_sequence: lastSequnce && lastSequnce.length > 0 ? lastSequnce : null,
         },
       })
       .then((res) => {
         const set = res.data?.records?.alarms || [];
-        
+
         if (set.length > 0 && !playing) {
           toggle();
         }
 
         const payload = [...alarms, ...set];
         const unique = _.uniqBy(payload, 'seq');
-        const filtered = seen.length > 0 ? _.filter(unique, (object) => {
-          return !seen.includes(`${object?.gen_date}-${object?.message}`);
-        }) : unique;
-        
+        const filtered =
+          seen.length > 0
+            ? _.filter(unique, (object) => {
+                return !seen.includes(`${object?.gen_date}-${object?.message}`);
+              })
+            : unique;
+
         setAlarms(filtered);
         sessionStorage.setItem('alarms', JSON.stringify(filtered));
-        if (set.length > 0 && 
-          (!lastSequnce || (res.data && parseInt(res.data.records.last_sequence) > parseInt(lastSequnce)))) {
-          setLastSequence(res.data?.records?.last_sequence)
+        if (
+          set.length > 0 &&
+          (!lastSequnce || (res.data && parseInt(res.data.records.last_sequence) > parseInt(lastSequnce)))
+        ) {
+          setLastSequence(res.data?.records?.last_sequence);
         }
       })
       .catch((errors) => {
-        console.log("Error, ", errors)
-      })
-    }
+        console.log('Error, ', errors);
+      });
+  };
 
   //
   useEffect(() => {
     const interval = setInterval(retrieveAlarms, refreshAlarm);
     return () => clearInterval(interval);
-  }, [alarms, seen])
+  }, [alarms, seen]);
 
   const menu = (
     <Menu style={{ minWidth: 200 }}>
@@ -132,36 +138,38 @@ const Events = () => {
         </Button>
       </div>
 
-      <Scrollbars
-        style={{
-          height: 'calc(100vh - 235px)',
-          width: '25vw',
-          marginTop: 5,
-          padding: 5,
-        }}
-      >
-        <List
-          style={{ width: '100%' }}
-          itemLayout="horizontal"
-          dataSource={alarms}
-          size="small"
-          renderItem={(item) => (
-            <List.Item
-              actions={[
-                <Button
-                  type="danger"
-                  size="small"
-                  icon={<CloseOutlined />}
-                  onClick={() => onRemove(`${item?.gen_date}-${item?.message}`)}
-                  shape="circle"
-                ></Button>,
-              ]}
-            >
-              <List.Item.Meta title={item.gen_date} description={item.message} />
-            </List.Item>
-          )}
-        />
-      </Scrollbars>
+      <div>
+        <Scrollbars
+          style={{
+            height: 'calc(100vh - 235px)',
+            width: '25vw',
+            marginTop: 5,
+            padding: 5,
+          }}
+        >
+          <List
+            style={{ width: '100%' }}
+            itemLayout="horizontal"
+            dataSource={alarms}
+            size="small"
+            renderItem={(item) => (
+              <List.Item
+                actions={[
+                  <Button
+                    type="danger"
+                    size="small"
+                    icon={<CloseOutlined />}
+                    onClick={() => onRemove(`${item?.gen_date}-${item?.message}`)}
+                    shape="circle"
+                  ></Button>,
+                ]}
+              >
+                <List.Item.Meta title={item.gen_date} description={item.message} />
+              </List.Item>
+            )}
+          />
+        </Scrollbars>
+      </div>
     </Menu>
   );
 
