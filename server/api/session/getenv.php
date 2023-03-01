2797e1548af978ff84b0a6621521316004aaa14a
WeiCaoMelbourne <weicao.melbourne@gmail.com>
2023-03-02 10:46:59 +1100

    OM5K-10912 Missing GUI components due to failure to read settings from ConfigStore

--- server/api/session/getenv.php
+++ server/api/session/getenv.php
@@ -6,7 +6,12 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-include_once '../config/setups.php';
- 
-// echo "hello";
-echo json_encode($_SERVER, JSON_PRETTY_PRINT);
+// include_once '../config/setups.php';
+// echo json_encode($_SERVER, JSON_PRETTY_PRINT);
+echo json_encode(array(
+    "SITE_TEMPERATURE_UNIT" => $_SERVER['SITE_TEMPERATURE_UNIT'],
+    "SITE_DENSITY_UNIT" => $_SERVER['SITE_DENSITY_UNIT'],
+    "TEMP_COMP_REF_TEMPERATURE" => $_SERVER['TEMP_COMP_REF_TEMPERATURE'],
+    "VSM_COMPENSATION_PT" => $_SERVER['VSM_COMPENSATION_PT'],
+    "SCHEDUNITS" => $_SERVER['SCHEDUNITS'],
+), JSON_PRETTY_PRINT);
