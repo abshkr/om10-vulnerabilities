60fef19ccc0ce06720916e4d0ec13d4c312a0a79
WeiCaoMelbourne <weicao.melbourne@gmail.com>
2023-12-14 13:34:56 +1100

    Removed stash folder because it is not for product, it is for dev only

--- server/api/stash/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include_once 'Request.php';
-include_once 'Router.php';
-include_once 'shared/log.php';
-
-write_log("Index.php invoked", __FILE__, __LINE__);
-
--- server/api/stash/phpinfo.php
+++
@@ -1,5 +0,0 @@
-
-<?php
-phpinfo();
-// $pay_load = check_token(get_http_token());
-// echo json_decode($pay_load);
